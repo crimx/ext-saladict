0bafdb59377f1856b83dab5002f91eb7b72c655a
CRIMX <straybugs@gmail.com>
2017-03-22 03:00:06 +0800

    Added default config

---
+++ src/app-config.js
@@ -0,0 +1,18 @@
+var config = () => ({
+  active: true,
+
+  // icon: show pop icon first
+  // direct: show panel directly
+  // ctrl: show panel when double click ctrl + selection not empty
+  mode: 'icon',
+
+  // show panel when triple press ctrl
+  tripleCtrl: true,
+
+  language: {
+    chinese: true,
+    english: false
+  }
+})
+
+export default config()
