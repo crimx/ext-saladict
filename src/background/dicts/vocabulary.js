b1c06ef5948d0203759ba73c56b8285b13d228d3
CRIMX <straybugs@gmail.com>
2017-04-03 18:13:08 +0800

    Added description

--- src/background/dicts/vocabulary.js
+++ src/background/dicts/vocabulary.js
@@ -2,12 +2,21 @@ import fetchDom from 'src/helpers/fetch-dom'
 
 export default function search (text, config) {
   return new Promise((resolve, reject) => {
-
     fetchDom('https://www.vocabulary.com/dictionary/' + text)
       .then(handleDom, reject)
       .then(resolve, reject)
       .catch(reject)
 
+    /**
+    * @typedef {Object} VocabularyResult
+    * @property {string} short
+    * @property {string} long
+    */
+
+    /**
+     * @async
+     * @returns {Promise.<VocabularyResult>} A promise with the result to send back
+     */
     function handleDom (doc) {
       let result = {}
 
