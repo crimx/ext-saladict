e9e0b48dbd2fa05c13b3f5731faa8a609bb5b3c6
CRIMX <straybugs@gmail.com>
2017-11-29 00:00:33 +0800

    Added animation

--- src/components/WordPage.vue
+++ src/components/WordPage.vue
@@ -64,35 +64,41 @@
           </ul>
         </nav>
       </div>
-      <div class="wordcard" v-if="wordAsideData" :style="{top: wordAsideTop + 'px'}">
+      <div class="wordcard" ref="wordcard" v-if="wordcardData" :style="{transform: `translateY(${wordcardTop}px)`}">
         <svg class="icon-edit" @click="openEditNotePanel" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 60 60">
           <path d="M36.72 22h-25c-.553 0-1 .448-1 1s.447 1 1 1h25c.55 0 1-.448 1-1s-.45-1-1-1zm-25-6h10c.55 0 1-.448 1-1s-.45-1-1-1h-10c-.553 0-1 .448-1 1s.447 1 1 1zm25 14h-25c-.553 0-1 .448-1 1s.447 1 1 1h25c.55 0 1-.448 1-1s-.45-1-1-1zm-8 8h-17c-.553 0-1 .448-1 1s.447 1 1 1h17c.55 0 1-.448 1-1s-.45-1-1-1zm-3 10c.55 0 1-.448 1-1s-.45-1-1-1h-14c-.553 0-1 .448-1 1s.447 1 1 1h14z"/>
           <path d="M2.72 2h29v14h14v18h2V14.586L33.132 0H.72v60h29v-2h-27V2zm31 1.414L44.304 14H33.72V3.414z"/>
           <path d="M58.407 37.162l-2.85-2.85c-1.127-1.128-3.093-1.128-4.22 0l-15.392 15.39-2.12 7.78-.52.518c-.388.388-.39 1.014-.006 1.405l-.008.02.02-.005c.194.19.446.287.698.287.257 0 .513-.098.708-.293l.518-.52 7.78-2.12 15.39-15.39c.563-.565.874-1.314.874-2.112s-.31-1.547-.875-2.11zm-20.153 13.06L48.497 39.98l4.243 4.242-10.243 10.243-4.243-4.243zm-.903 1.925l3.22 3.22-4.424 1.207 1.207-4.427zM56.996 39.97l-2.84 2.838-4.242-4.243 2.84-2.84c.372-.37 1.02-.372 1.393 0l2.85 2.85c.186.187.29.434.29.697-.002.263-.104.51-.29.697z"/>
         </svg>
-        <section class="wordcard-section" v-if="wordAsideData.trans">
-          <svg class="wordcard-section-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 469.333 469.333">
-            <path d="M253.227 300.267L199.04 246.72l.64-.64c37.12-41.387 63.573-88.96 79.147-139.307h62.507V64H192V21.333h-42.667V64H0v42.453h238.293c-14.4 41.173-36.907 80.213-67.627 114.347-19.84-22.08-36.267-46.08-49.28-71.467H78.72c15.573 34.773 36.907 67.627 63.573 97.28l-108.48 107.2L64 384l106.667-106.667 66.347 66.347 16.213-43.413zM373.333 192h-42.667l-96 256h42.667l24-64h101.333l24 64h42.667l-96-256zm-56 149.333L352 248.853l34.667 92.48h-69.334z"/>
-          </svg>
-          <p>{{ wordAsideData.trans }}</p>
-        </section>
-        <section class="wordcard-section" v-if="wordAsideData.sentence">
-          <svg class="wordcard-section-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 95.333 95.332">
-            <path d="M 36.587 45.263 C 35.07 44.825 33.553 44.605 32.078 44.605 C 29.799 44.605 27.898 45.125 26.423 45.763 C 27.844 40.559 31.259 31.582 38.061 30.57 C 38.69 30.476 39.207 30.021 39.379 29.408 L 40.864 24.09 C 40.99 23.641 40.916 23.16 40.66 22.77 C 40.403 22.38 39.991 22.119 39.529 22.056 C 39.027 21.987 38.515 21.952 38.009 21.952 C 29.844 21.952 21.759 30.474 18.347 42.675 C 16.344 49.833 15.757 60.595 20.686 67.369 C 23.445 71.16 27.472 73.183 32.657 73.385 L 32.717 73.386 C 39.114 73.386 44.783 69.079 46.508 62.915 C 47.538 59.229 47.073 55.364 45.196 52.029 C 43.338 48.731 40.28 46.327 36.581 45.263 Z M 76.615 52.029 C 74.758 48.731 71.699 46.327 68.002 45.263 C 66.484 44.823 64.968 44.604 63.492 44.604 C 61.214 44.604 59.311 45.121 57.838 45.76 C 59.259 40.553 62.673 31.579 69.475 30.564 C 70.102 30.47 70.619 30.016 70.793 29.402 L 72.28 24.085 C 72.403 23.635 72.332 23.155 72.073 22.764 C 71.814 22.373 71.401 22.113 70.942 22.049 C 70.438 21.981 69.928 21.946 69.417 21.946 C 61.253 21.946 53.169 30.467 49.755 42.669 C 47.752 49.827 47.166 60.59 52.101 67.364 C 54.858 71.153 58.887 73.178 64.069 73.379 C 64.091 73.38 64.111 73.381 64.134 73.381 C 70.527 73.381 76.198 69.074 77.923 62.908 C 78.953 59.224 78.485 55.358 76.609 52.022 Z" style=""/>
-          </svg>
-          <p>{{ wordAsideData.sentence }}</p>
-        </section>
-        <section class="wordcard-section" v-if="wordAsideData.note">
-          <svg class="wordcard-section-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 35.738 35.738">
-            <path d="M0 35.667S11.596-1.403 35.738.117c0 0-2.994 4.85-10.55 6.416 0 0 3.517.43 6.368-.522 0 0-1.71 5.517-11.025 6.275 0 0 5.135 1.33 7.416.57 0 0-.62 4.11-10.102 6.154-.562.12-4.347 1.066-1.306 1.447 0 0 4.37.763 5.514.38 0 0-3.743 5.608-12.927 5.133-.903-.048-1.332 0-1.332 0L0 35.666z"/>
-          </svg>
-          <p class="wordcard-sec-note-content">{{ wordAsideData.note }}</p>
-        </section>
+        <div class="wordcard-section-wrap" :style="{height: wordcardTransHeight + 'px'}">
+          <section class="wordcard-section" ref="wordcardTrans" v-if="wordcardData.trans">
+            <svg class="wordcard-section-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 469.333 469.333">
+              <path d="M253.227 300.267L199.04 246.72l.64-.64c37.12-41.387 63.573-88.96 79.147-139.307h62.507V64H192V21.333h-42.667V64H0v42.453h238.293c-14.4 41.173-36.907 80.213-67.627 114.347-19.84-22.08-36.267-46.08-49.28-71.467H78.72c15.573 34.773 36.907 67.627 63.573 97.28l-108.48 107.2L64 384l106.667-106.667 66.347 66.347 16.213-43.413zM373.333 192h-42.667l-96 256h42.667l24-64h101.333l24 64h42.667l-96-256zm-56 149.333L352 248.853l34.667 92.48h-69.334z"/>
+            </svg>
+            <p>{{ wordcardData.trans }}</p>
+          </section>
+        </div>
+        <div class="wordcard-section-wrap":style="{height: wordcardSenHeight + 'px'}">
+          <section class="wordcard-section" ref="wordcardSen" v-if="wordcardData.sentence">
+            <svg class="wordcard-section-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 95.333 95.332">
+              <path d="M 36.587 45.263 C 35.07 44.825 33.553 44.605 32.078 44.605 C 29.799 44.605 27.898 45.125 26.423 45.763 C 27.844 40.559 31.259 31.582 38.061 30.57 C 38.69 30.476 39.207 30.021 39.379 29.408 L 40.864 24.09 C 40.99 23.641 40.916 23.16 40.66 22.77 C 40.403 22.38 39.991 22.119 39.529 22.056 C 39.027 21.987 38.515 21.952 38.009 21.952 C 29.844 21.952 21.759 30.474 18.347 42.675 C 16.344 49.833 15.757 60.595 20.686 67.369 C 23.445 71.16 27.472 73.183 32.657 73.385 L 32.717 73.386 C 39.114 73.386 44.783 69.079 46.508 62.915 C 47.538 59.229 47.073 55.364 45.196 52.029 C 43.338 48.731 40.28 46.327 36.581 45.263 Z M 76.615 52.029 C 74.758 48.731 71.699 46.327 68.002 45.263 C 66.484 44.823 64.968 44.604 63.492 44.604 C 61.214 44.604 59.311 45.121 57.838 45.76 C 59.259 40.553 62.673 31.579 69.475 30.564 C 70.102 30.47 70.619 30.016 70.793 29.402 L 72.28 24.085 C 72.403 23.635 72.332 23.155 72.073 22.764 C 71.814 22.373 71.401 22.113 70.942 22.049 C 70.438 21.981 69.928 21.946 69.417 21.946 C 61.253 21.946 53.169 30.467 49.755 42.669 C 47.752 49.827 47.166 60.59 52.101 67.364 C 54.858 71.153 58.887 73.178 64.069 73.379 C 64.091 73.38 64.111 73.381 64.134 73.381 C 70.527 73.381 76.198 69.074 77.923 62.908 C 78.953 59.224 78.485 55.358 76.609 52.022 Z" style=""/>
+            </svg>
+            <p>{{ wordcardData.sentence }}</p>
+          </section>
+        </div>
+        <div class="wordcard-section-wrap":style="{height: wordcardNoteHeight + 'px'}">
+          <section class="wordcard-section" ref="wordcardNote" v-if="wordcardData.note">
+            <svg class="wordcard-section-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 35.738 35.738">
+              <path d="M0 35.667S11.596-1.403 35.738.117c0 0-2.994 4.85-10.55 6.416 0 0 3.517.43 6.368-.522 0 0-1.71 5.517-11.025 6.275 0 0 5.135 1.33 7.416.57 0 0-.62 4.11-10.102 6.154-.562.12-4.347 1.066-1.306 1.447 0 0 4.37.763 5.514.38 0 0-3.743 5.608-12.927 5.133-.903-.048-1.332 0-1.332 0L0 35.666z"/>
+            </svg>
+            <p class="wordcard-sec-note-content">{{ wordcardData.note }}</p>
+          </section>
+        </div>
         <hr>
         <footer class="wordcard-footer">
-            <img :src="wordAsideData.faviconURL" v-if="wordAsideData.faviconURL" class="wordcard-favicon">
+            <img :src="wordcardData.faviconURL" v-if="wordcardData.faviconURL" class="wordcard-favicon">
           <p class="wordcard-title">
-            <a :href="wordAsideData.url" @click.prevent="openURL(wordAsideData.url)">{{ wordAsideData.title }}</a>
+            <a :href="wordcardData.url" @click.prevent="openURL(wordcardData.url)">{{ wordcardData.title }}</a>
           </p>
         </footer>
       </div>
@@ -137,32 +143,32 @@
               <div class="form-group">
                 <div class="form-group">
                   <label for="note-word-text">{{ i18n('edit_note_word') }}</label>
-                  <input class="form-control" placeholder="Word" id="note-word-text" v-model="wordAsideData.text">
+                  <input class="form-control" placeholder="Word" id="note-word-text" v-model="wordcardData.text">
                 </div>
               </div>
               <div class="form-group">
                 <label for="note-word-trans">{{ i18n('edit_note_trans') }}</label>
-                <textarea v-focus class="form-control" row="5" placeholder="Translation" id="note-word-trans" v-model="wordAsideData.trans"></textarea>
+                <textarea v-focus class="form-control" row="5" placeholder="Translation" id="note-word-trans" v-model="wordcardData.trans"></textarea>
               </div>
               <div class="form-group">
                 <label for="note-word-note">{{ i18n('edit_note_note') }}</label>
-                <textarea class="form-control" row="5" placeholder="Notes" id="note-word-note" v-model="wordAsideData.note"></textarea>
+                <textarea class="form-control" row="5" placeholder="Notes" id="note-word-note" v-model="wordcardData.note"></textarea>
               </div>
               <div class="form-group">
                 <label for="note-word-context">{{ i18n('edit_note_context') }}</label>
-                <textarea class="form-control" row="5" placeholder="Context" id="note-word-context" v-model="wordAsideData.sentence"></textarea>
+                <textarea class="form-control" row="5" placeholder="Context" id="note-word-context" v-model="wordcardData.sentence"></textarea>
               </div>
               <div class="form-group">
                 <label for="note-source-title">{{ i18n('edit_note_src_title') }}</label>
-                <input class="form-control" placeholder="Source" id="note-source-title" v-model="wordAsideData.title">
+                <input class="form-control" placeholder="Source" id="note-source-title" v-model="wordcardData.title">
               </div>
               <div class="form-group">
                 <label for="note-source-url">{{ i18n('edit_note_src_url') }}</label>
-                <input class="form-control" placeholder="Source URL" id="note-source-url" v-model="wordAsideData.url">
+                <input class="form-control" placeholder="Source URL" id="note-source-url" v-model="wordcardData.url">
               </div>
               <div class="form-group">
-                <label for="note-source-favicon">{{ i18n('edit_note_src_favicon') }}<img class="favicon-note-source" :src="wordAsideData.faviconURL" v-if="wordAsideData.faviconURL"></label>
-                <input class="form-control" placeholder="Source Favicon" id="note-source-favicon" v-model="wordAsideData.faviconURL">
+                <label for="note-source-favicon">{{ i18n('edit_note_src_favicon') }}<img class="favicon-note-source" :src="wordcardData.faviconURL" v-if="wordcardData.faviconURL"></label>
+                <input class="form-control" placeholder="Source Favicon" id="note-source-favicon" v-model="wordcardData.faviconURL">
               </div>
             </form>
           </div>
@@ -219,10 +225,14 @@ export default {
       pageCount: 0,
       isMergeFirstTwo: false, // if the first page is too short, merge with the second one
 
-      wordAsideTop: 0,
-      wordAsideData: null,
+      wordcardTop: 0,
+      wordcardData: null,
       iActiveRecord: 0,
       iActiveWord: 0,
+
+      wordcardTransHeight: 0,
+      wordcardSenHeight: 0,
+      wordcardNoteHeight: 0,
 
       isOnlyEng: false,
 
@@ -392,8 +402,8 @@ export default {
         .then(() => {
           this.rawRecords[iRecord].data.splice(iWord, 1)
           // reset aside card
-          this.wordAsideTop = 0
-          this.wordAsideData = null
+          this.wordcardTop = 0
+          this.wordcardData = null
           this.iActiveWord = 0
           this.iActiveRecord = 0
         })
@@ -426,19 +436,19 @@ export default {
       for (let node = event.currentTarget; node && node !== wordListWrap; node = node.offsetParent) {
         top += node.offsetTop
       }
-      this.wordAsideTop = top
+      this.wordcardTop = top
       this.iActiveRecord = iRecord
       this.iActiveWord = iWord
-      this.wordAsideData = word
+      this.wordcardData = word
     },
     openEditNotePanel () {
       // backup value
-      this.bakWordAsideData = JSON.stringify(this.wordAsideData)
+      this.bakWordAsideData = JSON.stringify(this.wordcardData)
       this.isShowEditNotePanel = true
     },
     closeEditNotePanel () {
       // restore value
-      this.wordAsideData = JSON.parse(this.bakWordAsideData)
+      this.wordcardData = JSON.parse(this.bakWordAsideData)
       this.isShowEditNotePanel = false
     },
     saveNote () {
@@ -446,7 +456,7 @@ export default {
         setId: this.records[this.iActiveRecord].setId,
         iRecord: this.iActiveRecord,
         iWord: this.iActiveWord,
-        word: this.wordAsideData
+        word: this.wordcardData
       }).then(() => {
         this.isShowEditNotePanel = false // close & not restore
         this.popup(chrome.i18n.getMessage('wordpage_save_success'), 'alert-success')
@@ -455,7 +465,7 @@ export default {
       })
     },
     saveNoteToNotebook () {
-      addRecord('notebook', this.wordAsideData)
+      addRecord('notebook', this.wordcardData)
         .then(() => {
           this.closeEditNotePanel() // close & restore
           this.popup(chrome.i18n.getMessage('wordpage_save_success'), 'alert-success')
@@ -485,10 +495,24 @@ export default {
       }
     },
     records (newVal, oldVal) {
-      // init wordAsideData
+      // init wordcardData
       if (newVal.length > 0 && oldVal.length <= 0) {
-        this.wordAsideData = newVal[0].data[0]
+        this.wordcardData = newVal[0].data[0]
       }
+    },
+    wordcardData (val) {
+      if (!val) { return }
+      this.$nextTick(() => {
+        this.wordcardTransHeight = this.$refs.wordcardTrans
+          ? this.$refs.wordcardTrans.offsetHeight
+          : 0
+        this.wordcardSenHeight = this.$refs.wordcardSen
+          ? this.$refs.wordcardSen.offsetHeight
+          : 0
+        this.wordcardNoteHeight = this.$refs.wordcardNote
+          ? this.$refs.wordcardNote.offsetHeight
+          : 0
+      })
     }
   },
   created () {
@@ -617,6 +641,8 @@ body {
   border-radius: 20px;
   border-top-left-radius: 0;
   background: #fcf8e3;
+  transition: all 0.4s;
+  will-change: transform, height;
 
   hr {
     margin: 8px;
@@ -634,6 +660,13 @@ body {
   cursor: pointer;
 }
 
+.wordcard-section-wrap {
+  overflow: hidden;
+  margin: 10px 0;
+  transition: all 0.4s;
+  will-change: height;
+}
+
 .wordcard-section-icon {
   position: absolute;
   top: -5px;
@@ -646,7 +679,6 @@ body {
 
 .wordcard-section {
   position: relative;
-  margin: 10px 0;
   padding: 0 10px 0 25px;
 }
 
