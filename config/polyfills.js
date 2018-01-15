c1cf797045aa1c558534f1fc7792f7c31433c276
CRIMX <straybugs@gmail.com>
2018-01-15 23:37:02 +0800

    Updated polyfill sinon-chrome

--- config/polyfills.js
+++ config/polyfills.js
@@ -1,9 +1,10 @@
 'use strict'
 
-window.browser = require('webextension-polyfill')
-
-// In tests, polyfill requestAnimationFrame since jsdom doesn't provide it yet.
-// We don't polyfill it in the browser--this is user's responsibility.
 if (process.env.NODE_ENV === 'test') {
+  window.browser = require('sinon-chrome/webextensions')
+  // In tests, polyfill requestAnimationFrame since jsdom doesn't provide it yet.
+  // We don't polyfill it in the browser--this is user's responsibility.
   require('raf').polyfill(global)
+} else {
+  window.browser = require('webextension-polyfill')
 }
