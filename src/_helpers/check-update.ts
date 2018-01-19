9fc669839772f09c1af709cf2ea814ce81a20dc3
CRIMX <straybugs@gmail.com>
2018-01-19 18:00:24 +0800

    Added check-update

--- src/_helpers/check-update.ts
+++ src/_helpers/check-update.ts
@@ -1,16 +1,18 @@
-/**
- * @returns {Promise<boolean>} isAvailable
- */
-export default function checkUpdate () {
+type UpdateInfo = {
+  isAvailable: boolean
+  info?: any
+}
+
+export default function checkUpdate (): Promise<UpdateInfo> {
   // check new version
   return fetch('https://api.github.com/repos/crimx/crx-saladict/releases/latest')
     .then(r => r.json())
     .then(data => {
       if (data && data.tag_name) {
         let vGithub = /\d+\.\d+\.\d+/.exec(data.tag_name)
-        if (!vGithub) { return }
+        if (!vGithub) { return { isAvailable: false } }
         let gits = vGithub[0].split('.').map(v => Number(v))
-        let curs = chrome.runtime.getManifest().version.split('.').map(v => Number(v))
+        let curs = browser.runtime.getManifest().version.split('.').map(v => Number(v))
         return {
           info: data,
           isAvailable: gits[0] !== curs[0]
@@ -20,10 +22,10 @@ export default function checkUpdate () {
               : gits[2] > curs[2]
         }
       }
-      return {isAvailable: false}
+      return { isAvailable: false }
     })
     .catch(() => {
       console.warn('version check failed')
-      return {isAvailable: false}
+      return { isAvailable: false }
     })
 }
