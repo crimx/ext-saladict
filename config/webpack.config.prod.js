2c2d6f17e5be8cbe177a1060e55d4582aee891ba
CRIMX <straybugs@gmail.com>
2018-02-05 02:11:24 +0800

    build(webpack): add vue extension

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -103,7 +103,7 @@ module.exports = {
     // https://github.com/facebookincubator/create-react-app/issues/290
     // `web` extension prefixes have been added for better support
     // for React Native Web.
-    extensions: ['.ts', '.tsx', '.js', '.json', '.jsx'],
+    extensions: ['.ts', '.tsx', '.js', '.json', '.jsx', '.vue'],
     alias: {
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
