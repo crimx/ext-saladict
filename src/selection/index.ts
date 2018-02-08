2d63425bcc597c2ad7f6a77cdc4ee9410e2a743a
CRIMX <straybugs@gmail.com>
2018-02-09 00:59:53 +0800

    refactor(selection): rewrite selection

--- src/selection/index.ts
+++ src/selection/index.ts
@@ -1,215 +1,170 @@
-/**
- * Listen to mouse selection
- * Apply to all frames
- */
+import { appConfigFactory, AppConfig } from 'src/app-config'
+import { message, storage } from '@/_helpers/browser-api'
+import { isContainChinese, isContainEnglish } from '@/_helpers/lang-check'
+import { createAppConfigStream } from '@/_helpers/config-manager'
+import * as selection from '@/_helpers/selection'
+import { MsgSALADICT_SELECTION, MsgSELECTION } from '@/_typings/message'
 
-import {message, storage} from 'src/helpers/chrome-api'
-import {isContainChinese, isContainEnglish} from 'src/helpers/lang-check'
-import {hasSelection, getSelectionText, getSelectionSentence, getSelectionInfo} from 'src/helpers/selection'
-import AppConfig from 'src/app-config'
+import { Observable } from 'rxjs/Observable'
+import { fromPromise } from 'rxjs/observable/fromPromise'
+import { of } from 'rxjs/observable/of'
+import { map, distinctUntilChanged, filter, withLatestFrom, buffer, debounceTime, partition, observeOn } from 'rxjs/operators'
+import { fromEvent } from 'rxjs/observable/fromEvent'
+import { merge } from 'rxjs/observable/merge'
+import { async } from 'rxjs/scheduler/async.js'
 
-let config = new AppConfig()
-let numTripleCtrl = 0
-let tripleCtrlTimeout = null
-let isCtrlKeydown = false
-
-storage.sync.get('config', data => {
-  if (data.config) {
-    config = data.config
-  }
-
-  if (config.tripleCtrl) {
-    document.addEventListener('keyup', handleTripleCtrlKeyup)
-  }
+message.addListener('__PRELOAD_SELECTION__', (data, sender, sendResponse) => {
+  sendResponse(selection.getSelectionInfo())
 })
 
-storage.sync.listen('config', ({config: {newValue, oldValue}}) => {
-  if (newValue.tripleCtrl) {
-    if (!oldValue.tripleCtrl) {
-      document.addEventListener('keyup', handleTripleCtrlKeyup)
-    }
-  } else {
-    if (oldValue.tripleCtrl) {
-      document.removeEventListener('keyup', handleTripleCtrlKeyup)
-    }
-  }
-})
-
-document.addEventListener('keydown', evt => {
-  if (isCtrl(evt)) {
-    isCtrlKeydown = true
-  }
-})
-
-document.addEventListener('keyup', evt => {
-  isCtrlKeydown = false
-})
-
-// Reset state when user switching window with ctrl/meta key
-window.addEventListener('blur', () => {
-  isCtrlKeydown = false
-})
-
-document.addEventListener('mouseup', handleMouseup, true)
-
-window.addEventListener('message', handleFrameMsg, false)
-
-message.on('__PRELOAD_SELECTION__', (data, sender, sendResponse) => {
-  sendResponse(getSelectionInfo())
-})
-
-/**
- * send when hits ctrl button three times
- * @param {MouseEvent} evt
- */
-function handleTripleCtrlKeyup (evt) {
-  // ctrl & command(mac)
-  if (isCtrl(evt)) {
-    if (++numTripleCtrl === 3) {
-      if (!config.tripleCtrl) { return }
-      message.self.send({msg: 'TRIPLE_CTRL'})
-    } else {
-      if (tripleCtrlTimeout) { clearTimeout(tripleCtrlTimeout) }
-      tripleCtrlTimeout = setTimeout(() => {
-        numTripleCtrl = 0
-        tripleCtrlTimeout = null
-      }, 500)
-    }
-  }
-}
-
-/**
- * handle mouseup, send selection to dict panal
- * @param {MouseEvent} event
- */
-function handleMouseup ({button, target, clientX, clientY}) {
-  if (button !== 0 || // 0: Main button pressed, usually the left button
-      !config.active ||
-      window.name === 'saladict-frame' ||
-      target.className.startsWith('saladict-')
-  ) {
-    return // ignore mouse events on dict panal
-  }
-
-  if (!hasSelection()) {
-    // empty message
-    return message.self.send({
-      msg: 'SELECTION',
-      selectionInfo: {
-        text: '',
-        context: '',
-        title: window.pageTitle || document.title,
-        url: window.pageURL || document.URL,
-        // set by chrome-api helper
-        favicon: window.faviconURL || '',
-        trans: '',
-        note: ''
-      }
-    })
-  }
-
-  // if user click on a selected text,
-  // getSelection would reture the text before it disappears
-  // delay to wait for selection get cleared
-  setTimeout(() => {
-    const text = getSelectionText()
-    if (
-      text &&
-      (
-        (config.language.english && isContainEnglish(text) && !isContainChinese(text)) ||
-        (config.language.chinese && isContainChinese(text))
-      )
-    ) {
-      const selectionInfo = {
-        text,
-        context: getSelectionSentence(),
-        title: window.pageTitle || document.title,
-        url: window.pageURL || document.URL,
-        favicon: window.faviconURL || '',
-        trans: '',
-        note: ''
-      }
-
-      if (window.parent === window) {
-        // top
-        message.self.send({
-          msg: 'SELECTION',
-          selectionInfo,
-          mouseX: clientX,
-          mouseY: clientY,
-          ctrlKey: isCtrlKeydown
-        })
-      } else {
-        // post to upper frames/window
-        window.parent.postMessage({
-          msg: 'SALADICT_SELECTION',
-          selectionInfo,
-          mouseX: clientX,
-          mouseY: clientY,
-          ctrlKey: isCtrlKeydown
-        }, '*')
-      }
-    } else {
-      // empty message
-      return message.self.send({
-        msg: 'SELECTION',
-        selectionInfo: {
-          text: '',
-          context: '',
-          title: window.pageTitle || document.title,
-          url: window.pageURL || document.URL,
-          favicon: window.faviconURL || '',
-          trans: '',
-          note: ''
-        }
-      })
-    }
-  }, 0)
-}
-
-/**
- * handle message from iframes, add up coordinates
- * @param {MessageEvent} event
- */
-function handleFrameMsg ({data, source}) {
-  if (data.msg !== 'SALADICT_SELECTION') { return }
+window.addEventListener('message', ({ data, source }: { data: MsgSALADICT_SELECTION, source: Window }) => {
+  if (data.type !== 'SALADICT_SELECTION') { return }
 
   // get the souce iframe
   const iframe = Array.from(document.querySelectorAll('iframe'))
-    .find(({contentWindow}) => contentWindow === source)
+    .find(({ contentWindow }) => contentWindow === source)
   if (!iframe) { return }
 
-  let {selectionInfo, mouseX, mouseY, ctrlKey} = data
-  let {left, top} = iframe.getBoundingClientRect()
-  mouseX += left
-  mouseY += top
+  const { selectionInfo, mouseX, mouseY, ctrlKey } = data
+  const { left, top } = iframe.getBoundingClientRect()
 
   if (window.parent === window) {
     // top
     message.self.send({
-      msg: 'SELECTION',
+      type: 'SELECTION',
       selectionInfo,
-      mouseX,
-      mouseY,
+      mouseX: mouseX + left,
+      mouseY: mouseY + top,
       ctrlKey
-    })
+    } as MsgSELECTION)
   } else {
     // post to upper frames/window
     window.parent.postMessage({
-      msg: 'SALADICT_SELECTION',
+      type: 'SALADICT_SELECTION',
       selectionInfo,
-      mouseX,
-      mouseY,
+      mouseX: mouseX + left,
+      mouseY: mouseY + top,
       ctrlKey
-    }, '*')
+    } as MsgSALADICT_SELECTION, '*')
   }
-}
+})
+
+const appConfig$: Observable<AppConfig> = createAppConfigStream()
+
+const isConfigActive$: Observable<boolean> = appConfig$.pipe(
+  map(config => config.active),
+)
+
+const isConfigTripleCtrl$: Observable<boolean> = appConfig$.pipe(
+  map(config => config.tripleCtrl),
+)
+
+const configLanguage$: Observable<AppConfig['language']> = appConfig$.pipe(
+  map(config => config.language),
+)
+
+const isCtrlPressed$: Observable<boolean> = merge(
+  fromEvent(window, 'keydown', true, e => isCtrlKey(e)),
+  fromEvent(window, 'keyup', true, e => false),
+  fromEvent(window, 'blur', true, e => false),
+)
+
+const ctrlPressed$ = isCtrlPressed$.pipe(
+  withLatestFrom(isConfigTripleCtrl$, (isCtrlPressed, isConfig) => isConfig && isCtrlPressed),
+  filter(isCtrlPressed => isCtrlPressed),
+)
+
+const tripleCtrlPressed$ = ctrlPressed$.pipe(
+  buffer(ctrlPressed$.pipe(debounceTime(500))),
+  map(group => group.length),
+  filter(x => x >= 3),
+)
+
+const mouseup$: Observable<MouseEvent> = fromEvent<MouseEvent>(window, 'mouseup', true).pipe(
+  withLatestFrom(isConfigActive$),
+  filter(([ e, isConfigActive ]) => {
+    if (!isConfigActive || window.name === 'saladict-frame') { return false }
+    if ((e.target as Element).className && ((e.target as Element).className.startsWith('saladict-'))) {
+      return false
+    }
+    return true
+  }),
+  map(([ e ]) => e),
+  // if user click on a selected text,
+  // getSelection would reture the text before the highlight disappears
+  // delay to wait for selection get cleared
+  observeOn(async),
+)
+
+const [ goodSelection$, badSelection$ ] = partition<[MouseEvent, string, AppConfig['language'], boolean]>(
+  ([ e, text, lang ]) => {
+    if (!selection.hasSelection()) { return false }
+    return (
+      (lang.english && isContainEnglish(text) && !isContainChinese(text)) ||
+      (lang.chinese && isContainChinese(text))
+    )
+  }
+)(mouseup$.pipe(
+  withLatestFrom(of(selection.getSelectionText()), configLanguage$, isCtrlPressed$)
+))
+
+tripleCtrlPressed$.subscribe(() => {
+  message.self.send({ type: 'TRIPLE_CTRL' })
+})
+
+goodSelection$.subscribe(([ { button, target, clientX, clientY }, text, lang, isCtrlPressed ]) => {
+  const selectionInfo = {
+    text,
+    context: selection.getSelectionSentence(),
+    title: window.pageTitle || document.title,
+    url: window.pageURL || document.URL,
+    favicon: window.faviconURL || '',
+    trans: '',
+    note: ''
+  }
+
+  if (window.parent === window) {
+    // top
+    message.self.send({
+      type: 'SELECTION',
+      selectionInfo,
+      mouseX: clientX,
+      mouseY: clientY,
+      ctrlKey: isCtrlPressed,
+    } as MsgSELECTION)
+  } else {
+    // post to upper frames/window
+    window.parent.postMessage({
+      type: 'SALADICT_SELECTION',
+      selectionInfo,
+      mouseX: clientX,
+      mouseY: clientY,
+      ctrlKey: isCtrlPressed,
+    } as MsgSALADICT_SELECTION, '*')
+  }
+})
+
+badSelection$.subscribe(() => {
+  // empty message
+  message.self.send({
+    type: 'SELECTION',
+    selectionInfo: {
+      text: '',
+      context: '',
+      title: window.pageTitle || document.title,
+      url: window.pageURL || document.URL,
+      // set by browser-api helper
+      favicon: window.faviconURL || '',
+      trans: '',
+      note: ''
+    }
+  } as MsgSELECTION)
+})
 
 /**
- * is ctrl button pressed
- * @param {MouseEvent} evt
+ * Is ctrl/command button pressed
  */
-function isCtrl (evt) {
+function isCtrlKey (evt: KeyboardEvent): boolean {
   // ctrl & command(mac)
   if (evt.keyCode) {
     if (evt.keyCode === 17 || evt.keyCode === 91 || evt.keyCode === 93) {
