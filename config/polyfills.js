50548e54209e308b724eb525a500a78de19ec2a2
CRIMX <straybugs@gmail.com>
2018-04-15 19:49:53 +0800

    test(package): add enzyme

--- config/polyfills.js
+++ config/polyfills.js
@@ -1,10 +1,5 @@
 'use strict'
 
-if (process.env.NODE_ENV === 'test') {
-  window.browser = require('sinon-chrome/extensions')
-  // In tests, polyfill requestAnimationFrame since jsdom doesn't provide it yet.
-  // We don't polyfill it in the browser--this is user's responsibility.
-  require('raf').polyfill(global)
-} else {
+if (process.env.NODE_ENV !== 'test') {
   window.browser = require('webextension-polyfill')
 }
