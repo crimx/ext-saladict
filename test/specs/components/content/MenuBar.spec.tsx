b2c735da229dd95bffbf98b8e65faa658755aff9
CRIMX <straybugs@gmail.com>
2018-04-28 02:40:03 +0800

    test(content): update test

--- test/specs/components/content/MenuBar.spec.tsx
+++ test/specs/components/content/MenuBar.spec.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { shallow } from 'enzyme'
 
 import { MenuBar } from '@/content/components/MenuBar'
+import { getDefaultSelectionInfo } from '@/_helpers/selection'
+import { MsgType, MsgSelection } from '@/typings/message'
 
 describe('Component/content/MenuBar', () => {
   it('should render correctly', () => {
@@ -10,7 +12,16 @@ describe('Component/content/MenuBar', () => {
       t: x => x,
       isFav: false,
       isPinned: false,
-      updateDragArea: noop,
+      selection: {
+        type: MsgType.Selection,
+        selectionInfo: getDefaultSelectionInfo(),
+        mouseX: 0,
+        mouseY: 0,
+        dbClick: false,
+        ctrlKey: false,
+      } as MsgSelection,
+      searchHistory: [],
+      handleDragStart: noop,
       searchText: noop,
       addToNotebook: noop,
       removeFromNotebook: noop,
@@ -27,7 +38,16 @@ describe('Component/content/MenuBar', () => {
       t: x => x,
       isFav: true,
       isPinned: true,
-      updateDragArea: noop,
+      selection: {
+        type: MsgType.Selection,
+        selectionInfo: getDefaultSelectionInfo(),
+        mouseX: 0,
+        mouseY: 0,
+        dbClick: false,
+        ctrlKey: false,
+      } as MsgSelection,
+      searchHistory: [],
+      handleDragStart: noop,
       searchText: noop,
       addToNotebook: noop,
       removeFromNotebook: noop,
