a17e8a87592b628fef83ed8f2b64ccaeb9d95b87
CRIMX <straybugs@gmail.com>
2018-05-10 17:12:50 +0800

    build(webpack): remove ts check on dev, too slow

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -265,7 +265,7 @@ module.exports = {
     ],
   },
   plugins: [
-    new ForkTsCheckerWebpackPlugin({tslint: true}),
+    // new ForkTsCheckerWebpackPlugin({tslint: true}),
     // Makes some environment variables available in index.html.
     // The public URL is available as %PUBLIC_URL% in index.html, e.g.:
     // <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico">
