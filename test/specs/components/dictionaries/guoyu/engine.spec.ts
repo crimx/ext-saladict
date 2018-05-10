5fc75286ff162ba4ed578a29c3bc75f956001a4b
CRIMX <straybugs@gmail.com>
2018-05-10 11:54:30 +0800

    test(dicts): update test

--- test/specs/components/dictionaries/guoyu/engine.spec.ts
+++ test/specs/components/dictionaries/guoyu/engine.spec.ts
@@ -1,16 +1,12 @@
-import search from '@/components/dictionaries/google/engine'
+import search from '@/components/dictionaries/guoyu/engine'
 import { appConfigFactory } from '@/app-config'
-import fs from 'fs'
-import path from 'path'
-
-const response = fs.readFileSync(path.join(__dirname, 'response/f.txt'), 'utf8')
 
 const fetchbak = window.fetch
 
 describe('Dict/Google/engine', () => {
   beforeAll(() => {
     window.fetch = jest.fn((url: string) => Promise.resolve({
-      text: () => response
+      json: () => require('./response/愛.json')
     }))
   })
 
@@ -21,8 +17,10 @@ describe('Dict/Google/engine', () => {
   it('should parse result correctly', () => {
     return search('any', appConfigFactory())
       .then(searchResult => {
-        expect(searchResult.audio).toBeUndefined()
-        expect(searchResult.result).toBe('不要温柔地进入那个晚安。 愤怒，对光明的消逝愤怒。')
+        expect(searchResult.audio && typeof searchResult.audio.py).toBe('string')
+        expect(typeof searchResult.result.t).toBe('string')
+        expect(Array.isArray(searchResult.result.h)).toBeTruthy()
+        expect(searchResult.result.translation).toBeTruthy()
       })
   })
 })
