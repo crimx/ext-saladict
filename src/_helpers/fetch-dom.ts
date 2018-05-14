511a12e7b4cbf0f50ee9d73e39429f9f876faa4f
CRIMX <straybugs@gmail.com>
2018-05-14 20:13:44 +0800

    refactor(helpers): simplify fetch-dom

--- src/_helpers/fetch-dom.ts
+++ src/_helpers/fetch-dom.ts
@@ -1,25 +1,19 @@
-type Options = {
-  method?: string
-  body?: null | Document | Blob | BufferSource | FormData | URLSearchParams | ReadableStream | USVString
+import DOMPurify from 'dompurify'
+
+export function fetchDOM (input?: string | Request, init?: RequestInit): Promise<Document> {
+  return fetch(input, init)
+    .then(r => r.text())
+    .then(text => new DOMParser().parseFromString(
+      DOMPurify.sanitize(text),
+      'text/html',
+    ))
 }
 
-export default function fetchDOM (url: string, options?: Options): Promise<Document> {
-  url = encodeURI(url.replace('\n', ' '))
-
-  const method = (options && options.method && options.method.toLowerCase() === 'post') ? 'POST' : 'GET'
-  const body = options && options.body
-
-  return new Promise((resolve, reject) => {
-    let xhr = new XMLHttpRequest()
-    xhr.onload = () => {
-      if (!xhr.responseXML) {
-        return reject(new Error('Empty body, Response code ' + xhr.status))
-      }
-      resolve(xhr.responseXML)
-    }
-    xhr.onerror = reject
-    xhr.open(method, url)
-    xhr.responseType = 'document'
-    return body ? xhr.send(body) : xhr.send()
-  })
+export function fetchDirtyDOM (input?: string | Request, init?: RequestInit): Promise<Document> {
+  return fetch(input, init)
+    .then(r => r.text())
+    .then(text => new DOMParser().parseFromString(
+      text,
+      'text/html',
+    ))
 }
