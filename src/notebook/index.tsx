b24c7db1e508153ed02621cd5f6311761a4a1afc
CRIMX <straybugs@gmail.com>
2018-05-18 23:10:26 +0800

    feat(notebook): add notebook entry

---
+++ src/notebook/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import WordPage from '@/components/WordPage'
+
+window['__SALADICT_INTERNAL_PAGE__'] = true
+
+ReactDOM.render(<WordPage area='notebook' />, document.getElementById('root'))
+
+const $scriptSelection = document.createElement('script')
+$scriptSelection.src = './selection.js'
+$scriptSelection.type = 'text/javascript'
+
+const $scriptContent = document.createElement('script')
+$scriptContent.src = './content.js'
+$scriptContent.type = 'text/javascript'
+
+document.body.appendChild($scriptSelection)
+document.body.appendChild($scriptContent)
