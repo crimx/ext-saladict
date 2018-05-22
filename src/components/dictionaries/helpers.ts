09f929ff3e97a230eb0e9cf149e212d3d789ebdf
CRIMX <straybugs@gmail.com>
2018-05-23 02:00:01 +0800

    feat(dicts): add helper

--- src/components/dictionaries/helpers.ts
+++ src/components/dictionaries/helpers.ts
@@ -1,3 +1,9 @@
 export function handleNoResult<T> (): Promise<T> {
   return Promise.reject('No result')
 }
+
+export function getText (parent: ParentNode, selector?: string): string {
+  const child = selector ? parent.querySelector(selector) : parent
+  if (!child) { return '' }
+  return child['textContent'] || ''
+}
