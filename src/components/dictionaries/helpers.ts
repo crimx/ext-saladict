428cd082a05a897689d913f1c4c3ed413c846ece
CRIMX <straybugs@gmail.com>
2018-05-26 12:54:30 +0800

    feat(dicts): add helpers

--- src/components/dictionaries/helpers.ts
+++ src/components/dictionaries/helpers.ts
@@ -1,3 +1,7 @@
+import DOMPurify from 'dompurify'
+
+export type HTMLString = string
+
 export function handleNoResult<T> (): Promise<T> {
   return Promise.reject('No result')
 }
@@ -6,4 +10,32 @@ export function getText (parent: ParentNode, selector?: string): string {
   const child = selector ? parent.querySelector(selector) : parent
   if (!child) { return '' }
   return child['textContent'] || ''
+}
+
+export function getInnerHTMLThunk (host?: string) {
+  if (host && !host.endsWith('/')) {
+    host = host + '/'
+  }
+  return function getInnerHTML (parent: ParentNode, selector?: string): HTMLString {
+    const child = selector ? parent.querySelector(selector) : parent
+    if (!child) { return '' }
+    const content = DOMPurify.sanitize(child['innerHTML'] || '')
+    return host
+      ? content.replace(/href="\//g, 'href="' + host)
+      : content
+  }
+}
+
+export function getOuterHTMLThunk (host?: string) {
+  if (host && !host.endsWith('/')) {
+    host = host + '/'
+  }
+  return function getOuterHTML (parent: ParentNode, selector?: string): HTMLString {
+    const child = selector ? parent.querySelector(selector) : parent
+    if (!child) { return '' }
+    const content = DOMPurify.sanitize(child['outerHTML'] || '')
+    return host
+      ? content.replace(/href="\//g, 'href="' + host)
+      : content
+  }
 }
