dd44b75b57ba717da5e52453af6e3a6fbaa9a7c6
CRIMX <straybugs@gmail.com>
2018-06-05 12:43:45 +0800

    refactor(panel): fix Chrome flickering while preserving animation

--- src/content/components/DictItem/index.tsx
+++ src/content/components/DictItem/index.tsx
@@ -253,8 +253,8 @@ export class DictItem extends React.PureComponent<DictItemProps & { t: Translati
             </svg>
           }
           <button className={'panel-DictItem_FoldArrowBtn'} onClick={this.blurAfterClick}>
-            <svg className={`panel-DictItem_FoldArrow ${isUnfold ? 'isActive' : ''}`} width='18' height='18' viewBox='0 0 59.414 59.414' xmlns='http://www.w3.org/2000/svg'>
-              <path d='M43.854 59.414L14.146 29.707 43.854 0l1.414 1.414-28.293 28.293L45.268 58' />
+            <svg className='panel-DictItem_FoldArrow' width='18' height='18' viewBox='0 0 59.414 59.414' xmlns='http://www.w3.org/2000/svg'>
+              <path className={`panel-DictItem_FoldArrowPath ${isUnfold ? 'isActive' : ''}`} d='M43.854 59.414L14.146 29.707 43.854 0l1.414 1.414-28.293 28.293L45.268 58' />
             </svg>
           </button>
         </header>
