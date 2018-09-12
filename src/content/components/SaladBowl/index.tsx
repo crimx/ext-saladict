1d13ac56ea1ce220704bd256782f48bd4ced14e1
CRIMX <straybugs@gmail.com>
2018-09-12 11:57:24 +0800

    refactor(content): imagus compatible

--- src/content/components/SaladBowl/index.tsx
+++ src/content/components/SaladBowl/index.tsx
@@ -1,10 +1,5 @@
 import React from 'react'
 
-const imgLeaf = require('@/assets/leaf.svg')
-const imgOrange = require('@/assets/orange.svg')
-const imgTomato = require('@/assets/tomato.svg')
-const imgBowl = require('@/assets/bowl.svg')
-
 export interface SaladBowlProps {
   readonly mouseOnBowl: (flag: boolean) => any
   readonly isAnimation: boolean
@@ -28,14 +23,15 @@ export default class SaladBowl extends React.PureComponent<SaladBowlProps> {
     const { isAnimation } = this.props
     return (
       <div className={'saladict-SaladBowl' + (isAnimation ? ' isAnimate' : '')}
-        key={'saladict-SaladBowl'}
+        role='img'
+        key='saladict-SaladBowl'
         onMouseEnter={this.handleMouseEnter}
         onMouseLeave={this.handleMouseLeave}
       >
-        <img className='saladict-SaladBowl_Leaf' src={imgLeaf} />
-        <img className='saladict-SaladBowl_Orange' src={imgOrange} />
-        <img className='saladict-SaladBowl_Tomato' src={imgTomato} />
-        <img className='saladict-SaladBowl_Bowl' src={imgBowl} />
+        <div className='saladict-SaladBowl_Leaf' />
+        <div className='saladict-SaladBowl_Orange' />
+        <div className='saladict-SaladBowl_Tomato' />
+        <div className='saladict-SaladBowl_Bowl' />
       </div>
     )
   }
