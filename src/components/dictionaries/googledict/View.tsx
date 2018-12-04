22b3b7df449ba3a8d5de4697122ed00386dad644
CRIMX <straybugs@gmail.com>
2018-12-04 15:26:00 +0800

    refactor: abstract static speaker hoc

--- src/components/dictionaries/googledict/View.tsx
+++ src/components/dictionaries/googledict/View.tsx
@@ -1,59 +1,11 @@
 import React from 'react'
 import { GoogleDictResult } from './engine'
-import { message } from '@/_helpers/browser-api'
-import { MsgType, MsgAudioPlay } from '@/typings/message'
+import withStaticSpeaker from '@/components/withStaticSpeaker'
 import { ViewPorps } from '@/components/dictionaries/helpers'
 
-export default class DictGoogleDict extends React.PureComponent<ViewPorps<GoogleDictResult>> {
-  _audioDelayTimeout: any
-
-  isAudioElement (evt: React.MouseEvent<HTMLDivElement>): boolean {
-    const target = (evt.target as HTMLElement)
-    const cls = target.classList
-    return cls && cls.contains('dictGoogleDict-Speaker')
-  }
-
-  handleDictMouseOver = (evt: React.MouseEvent<HTMLDivElement>) => {
-    if (this.isAudioElement(evt)) {
-      clearTimeout(this._audioDelayTimeout)
-      // React resuses synthetic event object
-      const target = evt.target as HTMLElement
-      this._audioDelayTimeout =
-        setTimeout(() => this.playAudio(target), 400)
-    }
-  }
-
-  handleDictMouseOut = (evt: React.MouseEvent<HTMLDivElement>) => {
-    if (this.isAudioElement(evt)) {
-      clearTimeout(this._audioDelayTimeout)
-    }
-  }
-
-  handleDictClick = (evt: React.MouseEvent<HTMLDivElement>) => {
-    if (this.isAudioElement(evt)) {
-      clearTimeout(this._audioDelayTimeout)
-      const target = evt.target as HTMLElement
-      target.blur()
-      this.playAudio(target)
-    }
-  }
-
-  playAudio = (target: HTMLElement) => {
-    const src = target.dataset.srcMp3
-    if (src) {
-      message.send<MsgAudioPlay>({ type: MsgType.PlayAudio, src })
-    }
-  }
-
-  render () {
-    return (
-      <div
-        className='dictGoogleDict-Entry'
-        onClick={this.handleDictClick}
-        onMouseOver={this.handleDictMouseOver}
-        onMouseOut={this.handleDictMouseOut}
-        dangerouslySetInnerHTML={{ __html: this.props.result.entry }}
-      />
-    )
-  }
-}
+export default withStaticSpeaker((props: ViewPorps<GoogleDictResult>) => (
+  <div
+    className='dictGoogleDict-Entry'
+    dangerouslySetInnerHTML={{ __html: props.result.entry }}
+  />
+), 'dictGoogleDict-Speaker')
--- src/components/dictionaries/longman/View.tsx
+++ src/components/dictionaries/longman/View.tsx
@@ -2,172 +2,124 @@ import React from 'react'
 import Speaker from '@/components/Speaker'
 import StarRates from '@/components/StarRates'
 import { LongmanResult, LongmanResultLex, LongmanResultRelated, LongmanResultEntry } from './engine'
-import { message } from '@/_helpers/browser-api'
-import { MsgType, MsgAudioPlay } from '@/typings/message'
 import { ViewPorps } from '@/components/dictionaries/helpers'
 
-export default class DictLongman extends React.PureComponent<ViewPorps<LongmanResult>> {
-  _audioDelayTimeout: any
+import withStaticSpeaker from '@/components/withStaticSpeaker'
 
-  isAudioElement (evt: React.MouseEvent<HTMLDivElement>): boolean {
-    const target = (evt.target as HTMLElement)
-    const cls = target.classList
-    return cls && cls.contains('dictLongman-Speaker')
+export default withStaticSpeaker((props: ViewPorps<LongmanResult>) => {
+  switch (props.result.type) {
+    case 'lex':
+      return renderLex(props.result)
+    case 'related':
+      return renderRelated(props.result)
+    default:
+      return null
   }
+}, 'dictLongman-Speaker')
 
-  handleDictMouseOver = (evt: React.MouseEvent<HTMLDivElement>) => {
-    if (this.isAudioElement(evt)) {
-      clearTimeout(this._audioDelayTimeout)
-      // React resuses synthetic event object
-      const target = evt.target as HTMLElement
-      this._audioDelayTimeout =
-        setTimeout(() => this.playAudio(target), 400)
-    }
-  }
+function renderEntry (entry: LongmanResultEntry) {
+  return (
+    <section key={entry.title.HWD + entry.title.HOMNUM} className='dictLongman-Entry'>
+      <header>
+        <div className='dictLongman-HeaderContainer'>
+          <h1 className='dictLongman-Title'>
+            <span className='dictLongman-Title_HWD'>{entry.title.HWD}</span>
+            <span className='dictLongman-Title_HYPHENATION'>{entry.title.HYPHENATION}</span>
+            <span className='dictLongman-Title_HOMNUM'>{entry.title.HOMNUM}</span>
+          </h1>
+          {
+            entry.level ?
+            (
+              <span title={entry.level.title} className='dictLongman-Level'>
+                <StarRates max={3} rate={entry.level.rate} className='dictLongman-Level_Rate' />
+              </span>
+            )
+            : null
+          }
+          {entry.freq && entry.freq.map(freq => (
+            <span key={freq.rank} className='dictLongman-FREQ' title={freq.title}>{freq.rank}</span>
+          ))}
+        </div>
+        <div className='dictLongman-HeaderContainer'>
+          <span className='dictLongman-Pos'>{entry.pos}</span>
+          <span className='dictLongman-Phsym'>{entry.phsym}</span>
+          {entry.prons && entry.prons.map(pron => (
+            <React.Fragment key={pron.pron}>
+              {pron.lang.toUpperCase()}: <Speaker src={pron.pron} />
+            </React.Fragment>
+          ))}
+          {entry.topic && (
+            <>
+              Topic: <a href={entry.topic.href} rel='nofollow'>{entry.topic.title}</a>
+            </>
+          )}
+        </div>
+      </header>
 
-  handleDictMouseOut = (evt: React.MouseEvent<HTMLDivElement>) => {
-    if (this.isAudioElement(evt)) {
-      clearTimeout(this._audioDelayTimeout)
-    }
-  }
+      {entry.senses.map(sen => (
+        <div key={sen} className='dictLongman-Sense' dangerouslySetInnerHTML={{ __html: sen }} />
+      ))}
 
-  handleDictClick = (evt: React.MouseEvent<HTMLDivElement>) => {
-    if (this.isAudioElement(evt)) {
-      clearTimeout(this._audioDelayTimeout)
-      const target = evt.target as HTMLElement
-      target.blur()
-      this.playAudio(target)
-    }
-  }
+      {entry.collocations &&
+        <div className='dictLongman-Box' dangerouslySetInnerHTML={{ __html: entry.collocations }} />
+      }
 
-  playAudio = (target: HTMLElement) => {
-    const src = target.dataset.srcMp3
-    if (src) {
-      message.send<MsgAudioPlay>({ type: MsgType.PlayAudio, src })
-    }
-  }
+      {entry.grammar &&
+        <div className='dictLongman-Box' dangerouslySetInnerHTML={{ __html: entry.grammar }} />
+      }
 
-  renderEntry (entry: LongmanResultEntry) {
-    return (
-      <section key={entry.title.HWD + entry.title.HOMNUM} className='dictLongman-Entry'>
-        <header>
-          <div className='dictLongman-HeaderContainer'>
-            <h1 className='dictLongman-Title'>
-              <span className='dictLongman-Title_HWD'>{entry.title.HWD}</span>
-              <span className='dictLongman-Title_HYPHENATION'>{entry.title.HYPHENATION}</span>
-              <span className='dictLongman-Title_HOMNUM'>{entry.title.HOMNUM}</span>
-            </h1>
-            {
-              entry.level ?
-              (
-                <span title={entry.level.title} className='dictLongman-Level'>
-                  <StarRates max={3} rate={entry.level.rate} className='dictLongman-Level_Rate' />
-                </span>
-              )
-              : null
-            }
-            {entry.freq && entry.freq.map(freq => (
-              <span key={freq.rank} className='dictLongman-FREQ' title={freq.title}>{freq.rank}</span>
-            ))}
+      {entry.thesaurus &&
+        <div className='dictLongman-Box' dangerouslySetInnerHTML={{ __html: entry.thesaurus }} />
+      }
+
+      {entry.examples && entry.examples.length > 0 && (
+        <>
+          <h2 className='dictLongman-Examples_Title'>Examples from the Corpus</h2>
+          {entry.examples.map(exa => (
+            <div key={exa} className='dictLongman-Examples' dangerouslySetInnerHTML={{ __html: exa }} />
+          ))}
+        </>
+      )}
+    </section>
+  )
+}
+
+function renderLex (result: LongmanResultLex) {
+  type Dicts = ['bussiness', 'contemporary'] | ['contemporary', 'bussiness']
+  // const dictTitle = {
+  //   contemporary: 'Longman Dictionary of Contemporary English',
+  //   bussiness: 'Longman Business Dictionary',
+  // }
+  const dicts: Dicts = result.bussinessFirst
+    ? ['bussiness', 'contemporary']
+    : ['contemporary', 'bussiness']
+
+  return (
+    <>
+      {result.wordfams &&
+        <div className='dictLongman-Wordfams' dangerouslySetInnerHTML={{ __html: result.wordfams }} />
+      }
+
+      {dicts.map((dict, index) => result[dict].length > 0
+        ? (
+          <div className='dictLongman-Dict' key={dict + index}>
+            {/* <h1 className='dictLongman-DictTitle'>
+              <span>- {dictTitle[dict]} -</span>
+            </h1> */}
+            {result[dict].map(renderEntry)}
           </div>
-          <div className='dictLongman-HeaderContainer'>
-            <span className='dictLongman-Pos'>{entry.pos}</span>
-            <span className='dictLongman-Phsym'>{entry.phsym}</span>
-            {entry.prons && entry.prons.map(pron => (
-              <React.Fragment key={pron.pron}>
-                {pron.lang.toUpperCase()}: <Speaker src={pron.pron} />
-              </React.Fragment>
-            ))}
-            {entry.topic && (
-              <>
-                Topic: <a href={entry.topic.href} rel='nofollow'>{entry.topic.title}</a>
-              </>
-            )}
-          </div>
-        </header>
+        )
+        : null
+      )}
+    </>
+  )
+}
 
-        {entry.senses.map(sen => (
-          <div key={sen} className='dictLongman-Sense' dangerouslySetInnerHTML={{ __html: sen }} />
-        ))}
-
-        {entry.collocations &&
-          <div className='dictLongman-Box' dangerouslySetInnerHTML={{ __html: entry.collocations }} />
-        }
-
-        {entry.grammar &&
-          <div className='dictLongman-Box' dangerouslySetInnerHTML={{ __html: entry.grammar }} />
-        }
-
-        {entry.thesaurus &&
-          <div className='dictLongman-Box' dangerouslySetInnerHTML={{ __html: entry.thesaurus }} />
-        }
-
-        {entry.examples && entry.examples.length > 0 && (
-          <>
-            <h2 className='dictLongman-Examples_Title'>Examples from the Corpus</h2>
-            {entry.examples.map(exa => (
-              <div key={exa} className='dictLongman-Examples' dangerouslySetInnerHTML={{ __html: exa }} />
-            ))}
-          </>
-        )}
-      </section>
-    )
-  }
-
-  renderLex (result: LongmanResultLex) {
-    type Dicts = ['bussiness', 'contemporary'] | ['contemporary', 'bussiness']
-    // const dictTitle = {
-    //   contemporary: 'Longman Dictionary of Contemporary English',
-    //   bussiness: 'Longman Business Dictionary',
-    // }
-    const dicts: Dicts = result.bussinessFirst
-      ? ['bussiness', 'contemporary']
-      : ['contemporary', 'bussiness']
-
-    return (
-      <>
-        {result.wordfams &&
-          <div className='dictLongman-Wordfams' dangerouslySetInnerHTML={{ __html: result.wordfams }} />
-        }
-
-        {dicts.map((dict, index) => result[dict].length > 0
-          ? (
-            <div className='dictLongman-Dict'
-              key={dict + index}
-              onClick={this.handleDictClick}
-              onMouseOver={this.handleDictMouseOver}
-              onMouseOut={this.handleDictMouseOut}
-            >
-              {/* <h1 className='dictLongman-DictTitle'>
-                <span>- {dictTitle[dict]} -</span>
-              </h1> */}
-              {result[dict].map(this.renderEntry)}
-            </div>
-          )
-          : null
-        )}
-      </>
-    )
-  }
-
-  renderRelated (result: LongmanResultRelated) {
-    return (
-      <>
-        <p>Did you mean:</p>
-        <ul className='dictLongman-Related' dangerouslySetInnerHTML={{ __html: result.list }} />
-      </>
-    )
-  }
-
-  render () {
-    const { result } = this.props
-    switch (result.type) {
-      case 'lex':
-        return this.renderLex(result)
-      case 'related':
-        return this.renderRelated(result)
-      default:
-        return null
-    }
-  }
+function renderRelated (result: LongmanResultRelated) {
+  return (
+    <>
+      <p>Did you mean:</p>
+      <ul className='dictLongman-Related' dangerouslySetInnerHTML={{ __html: result.list }} />
+    </>
+  )
 }
