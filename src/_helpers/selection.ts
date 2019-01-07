3fa2fb622154c903a62dbb2782dcb6211d58fbfc
CRIMX <straybugs@gmail.com>
2019-01-07 21:27:35 +0800

    perf: faster matching sentence head
    Closes #274

--- src/_helpers/selection.ts
+++ src/_helpers/selection.ts
@@ -31,75 +31,17 @@ export function getSelectionText (win = window): string {
   return ''
 }
 
-// match head                 a.b is ok             chars that ends a sentence
-const sentenceHeadTester = /((\.(?![\s.?!。？！…]))|[^.?!。？！…])+$/
-// match tail                                                       for "..."
-const sentenceTailTester = /^((\.(?![\s.?!。？！…]))|[^.?!。？！…])*([.?!。？！…]){0,3}/
-
 /** Returns the sentence containing the selection text */
 export function getSelectionSentence (win = window): string {
   const selection = win.getSelection()
   const selectedText = selection.toString()
   if (!selectedText.trim()) { return '' }
 
-  let sentenceHead = ''
-  let sentenceTail = ''
-
-  const anchorNode = selection.anchorNode
-  if (anchorNode.nodeType === Node.TEXT_NODE) {
-    let leadingText = anchorNode.textContent || ''
-    if (leadingText) {
-      leadingText = leadingText.slice(0, selection.anchorOffset)
-    }
-    for (let node = anchorNode.previousSibling; node; node = node.previousSibling) {
-      if (node.nodeType === Node.TEXT_NODE) {
-        leadingText = node.textContent + leadingText
-      } else if (node.nodeType === Node.ELEMENT_NODE) {
-        leadingText = (node as HTMLElement).innerText + leadingText
-      }
-    }
-
-    for (
-      let element = anchorNode.parentElement;
-      element && INLINE_TAGS.has(element.tagName.toLowerCase()) && element !== document.body;
-      element = element.parentElement
-    ) {
-      for (let el = element.previousElementSibling; el; el = el.previousElementSibling) {
-        leadingText = (el as HTMLElement).innerText + leadingText
-      }
-    }
-
-    sentenceHead = (leadingText.match(sentenceHeadTester) || [''])[0]
-  }
-
-  const focusNode = selection.focusNode
-  if (selection.focusNode.nodeType === Node.TEXT_NODE) {
-    let tailingText = selection.focusNode.textContent || ''
-    if (tailingText) {
-      tailingText = tailingText.slice(selection.focusOffset)
-    }
-    for (let node = focusNode.nextSibling; node; node = node.nextSibling) {
-      if (node.nodeType === Node.TEXT_NODE) {
-        tailingText += node.textContent
-      } else if (node.nodeType === Node.ELEMENT_NODE) {
-        tailingText += (node as HTMLElement).innerText
-      }
-    }
-
-    for (
-      let element = focusNode.parentElement;
-      element && INLINE_TAGS.has(element.tagName.toLowerCase()) && element !== document.body;
-      element = element.parentElement
-    ) {
-      for (let el = element.nextElementSibling; el; el = el.nextElementSibling) {
-        tailingText += (el as HTMLElement).innerText
-      }
-    }
-
-    sentenceTail = (tailingText.match(sentenceTailTester) || [''])[0]
-  }
-
-  return cleanText(sentenceHead + selectedText + sentenceTail)
+  return cleanText(
+    extractSentenceHead(selection.anchorNode, selection.anchorOffset) +
+    selectedText +
+    extractSentenceTail(selection.focusNode, selection.focusOffset)
+  )
 }
 
 export type SelectionInfo = Readonly<SelectionInfoMutable>
@@ -159,4 +101,87 @@ export function getSelectionInfo (config: Partial<SelectionInfo> = {}): Selectio
 
 function cleanText (text: string): string {
   return text.replace(/\s+/g, ' ').trim()
+}
+
+function extractSentenceHead (anchorNode: Node, anchorOffset: number): string {
+  if (anchorNode.nodeType === Node.TEXT_NODE) {
+    let leadingText = anchorNode.textContent || ''
+    if (leadingText) {
+      leadingText = leadingText.slice(0, anchorOffset)
+    }
+
+    // prev siblings
+    for (let node = anchorNode.previousSibling; node; node = node.previousSibling) {
+      if (node.nodeType === Node.TEXT_NODE) {
+        leadingText = (node.textContent || '') + leadingText
+      } else if (node.nodeType === Node.ELEMENT_NODE) {
+        leadingText = (node as HTMLElement).innerText + leadingText
+      }
+    }
+
+    // parent prev siblings
+    for (
+      let element = anchorNode.parentElement;
+      element && INLINE_TAGS.has(element.tagName.toLowerCase()) && element !== document.body;
+      element = element.parentElement
+    ) {
+      for (let el = element.previousElementSibling; el; el = el.previousElementSibling) {
+        leadingText = (el as HTMLElement).innerText + leadingText
+      }
+    }
+
+    const puncTester = /[.?!。？！…]/
+    /** meaningful char after dot "." */
+    const charTester = /[\s.?!。？！…]/
+
+    for (let i = leadingText.length - 1; i >= 0; i--) {
+      const c = leadingText[i]
+      if (puncTester.test(c)) {
+        if (c === '.' && charTester.test(c)) {
+          // a.b is allowed
+          continue
+        }
+        return leadingText.slice(i + 1)
+      }
+    }
+
+    return leadingText
+  }
+
+  return ''
+}
+
+function extractSentenceTail (focusNode: Node, focusOffset: number): string {
+  if (focusNode.nodeType === Node.TEXT_NODE) {
+    let tailingText = focusNode.textContent || ''
+    if (tailingText) {
+      tailingText = tailingText.slice(focusOffset)
+    }
+
+    // next siblings
+    for (let node = focusNode.nextSibling; node; node = node.nextSibling) {
+      if (node.nodeType === Node.TEXT_NODE) {
+        tailingText += node.textContent
+      } else if (node.nodeType === Node.ELEMENT_NODE) {
+        tailingText += (node as HTMLElement).innerText
+      }
+    }
+
+    // parent next siblings
+    for (
+      let element = focusNode.parentElement;
+      element && INLINE_TAGS.has(element.tagName.toLowerCase()) && element !== document.body;
+      element = element.parentElement
+    ) {
+      for (let el = element.nextElementSibling; el; el = el.nextElementSibling) {
+        tailingText += (el as HTMLElement).innerText
+      }
+    }
+
+    // match tail                                                       for "..."
+    const sentenceTailTester = /^((\.(?![\s.?!。？！…]))|[^.?!。？！…])*([.?!。？！…]){0,3}/
+    return (tailingText.match(sentenceTailTester) || [''])[0]
+  }
+
+  return ''
 }
