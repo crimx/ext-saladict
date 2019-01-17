3541a32b981c47ff8237a5e1f009bd9d0c2bde20
CRIMX <straybugs@gmail.com>
2019-01-17 21:12:34 +0800

    refactor(options): add page General

---
+++ src/options/components/options/General/index.tsx
@@ -0,0 +1,76 @@
+import React from 'react'
+import { openURL } from '@/_helpers/browser-api'
+import { Props } from '../typings'
+import { updateConfig as updateConfigWrap } from '../helpers'
+
+import { FormComponentProps } from 'antd/lib/form'
+import { Form, Select, Switch, Button } from 'antd'
+
+const isChrome = navigator.userAgent.includes('Chrome')
+
+export class General extends React.Component<Props & FormComponentProps> {
+  openShortcuts = () => {
+    openURL('chrome://extensions/shortcuts')
+  }
+
+  render () {
+    const { t, config } = this.props
+    const updateConfig = updateConfigWrap(config)
+
+    const formItemLayout = {
+      labelCol: { span: 5 },
+      wrapperCol: { span: 8 },
+    }
+
+    return (
+      <Form>
+        {isChrome && // @todo wait for firefox https://bugzilla.mozilla.org/show_bug.cgi?id=1303384
+          <Button onClick={this.openShortcuts}>{t('opt_shortcuts')}</Button>
+        }
+        <Form.Item
+          {...formItemLayout}
+          label={t('opt_active')}
+          help={t('opt_app_active_help')}
+        >{
+          this.props.form.getFieldDecorator('active', {
+            initialValue: config.active,
+            getValueFromEvent: updateConfig('active'),
+            valuePropName: 'checked',
+          })(
+            <Switch />
+          )
+        }</Form.Item>
+        <Form.Item
+          {...formItemLayout}
+          label={t('opt_animation')}
+          help={t('opt_animation_help')}
+        >{
+          this.props.form.getFieldDecorator('animation', {
+            initialValue: config.animation,
+            getValueFromEvent: updateConfig('animation'),
+            valuePropName: 'checked',
+          })(
+            <Switch />
+          )
+        }</Form.Item>
+        <Form.Item
+          {...formItemLayout}
+          label={t('opt_language')}
+        >{
+          this.props.form.getFieldDecorator('langCode', {
+            initialValue: config.langCode,
+            getValueFromEvent: updateConfig('langCode'),
+          })(
+            <Select>
+              <Select.Option value='zh-CN'>简体中文</Select.Option>
+              <Select.Option value='zh-TW'>繁體中文</Select.Option>
+              <Select.Option value='en'>English</Select.Option>
+            </Select>
+          )
+        }</Form.Item>
+      </Form>
+    )
+  }
+}
+
+export default Form.create({})(General)
