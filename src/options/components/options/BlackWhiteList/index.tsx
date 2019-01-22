9b1c0fc20b614513384a1e562317dbf076eb8ef0
CRIMX <straybugs@gmail.com>
2019-01-22 18:49:36 +0800

    refactor(options): add page balckwhitelist and pdf

---
+++ src/options/components/options/BlackWhiteList/index.tsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import { Props } from '../typings'
+import { formItemLayout } from '../helpers'
+import MatchPatternModal from '../../MatchPatternModal'
+
+import { FormComponentProps } from 'antd/lib/form'
+import { Form, Button } from 'antd'
+
+export type BlackWhiteListProps = Props & FormComponentProps
+
+interface BlackWhiteListState {
+  editingArea: '' | 'pdfWhitelist' | 'pdfBlacklist' | 'whitelist' | 'blacklist'
+}
+
+export class BlackWhiteList extends React.Component<BlackWhiteListProps, BlackWhiteListState> {
+  constructor (props: BlackWhiteListProps) {
+    super(props)
+    this.state = {
+      editingArea: ''
+    }
+  }
+
+  closeModal = () => {
+    this.setState({ editingArea: '' })
+  }
+
+  render () {
+    const { t, config } = this.props
+
+    return (
+      <Form>
+        <Form.Item
+          {...formItemLayout}
+          label={t('opt_sel_blackwhitelist')}
+          help={t('opt_sel_blackwhitelist_help')}
+        >
+          <Button
+            style={{ marginRight: 10 }}
+            onClick={() => this.setState({ editingArea: 'blacklist' })}
+          >{t('common:blacklist')}</Button>
+          <Button
+            onClick={() => this.setState({ editingArea: 'whitelist' })}
+          >{t('common:whitelist')}</Button>
+        </Form.Item>
+        <Form.Item
+          {...formItemLayout}
+          label={`PDF ${t('nav_BlackWhiteList')}`}
+          help={t('opt_pdf_blackwhitelist_help')}
+        >
+          <Button
+            style={{ marginRight: 10 }}
+            onClick={() => this.setState({ editingArea: 'pdfBlacklist' })}
+          >PDF {t('common:blacklist')}</Button>
+          <Button
+            onClick={() => this.setState({ editingArea: 'pdfWhitelist' })}
+          >PDF {t('common:whitelist')}</Button>
+        </Form.Item>
+        <MatchPatternModal
+          t={t}
+          config={config}
+          area={this.state.editingArea}
+          onClose={this.closeModal}
+        />
+      </Form>
+    )
+  }
+}
+
+export default BlackWhiteList
