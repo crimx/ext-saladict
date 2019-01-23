2abc24a2a78e2409cc066255019b4ccb0222334d
CRIMX <straybugs@gmail.com>
2019-01-23 18:01:35 +0800

    fix: disable warning on dev

--- src/background/initialization.ts
+++ src/background/initialization.ts
@@ -132,16 +132,17 @@ function onStartup (): void {
       }
 
       // anti piracy
-      if (lastCheckUpdate && isExtTainted &&
-        Math.floor((today - lastCheckUpdate) / 24 / 60 / 60 / 1000) % 7 === 0
-      ) {
-        browser.notifications.create('update', {
-          type: 'basic',
-          iconUrl: browser.runtime.getURL(`static/icon-128.png`),
-          title: decodeURI('%E6%B2%99%E6%8B%89%E6%9F%A5%E8%AF%8D'),
-          message: decodeURI('%E6%AD%A4%E3%80%8C%E6%B2%99%E6%8B%89%E6%9F%A5%E8%AF%8D%E3%80%8D%E6%89%A9%E5%B1%95%E5%B7%B2%E8%A2%AB%E4%BA%8C%E6%AC%A1%E6%89%93%E5%8C%85%EF%BC%8C%E8%AF%B7%E5%9C%A8%E5%AE%98%E6%96%B9%E5%BB%BA%E8%AE%AE%E7%9A%84%E5%B9%B3%E5%8F%B0%E5%AE%89%E8%A3%85%E3%80%82'),
-          buttons: [{ title: decodeURI('%E6%9F%A5%E7%9C%8B%E5%8F%AF%E9%9D%A0%E7%9A%84%E5%B9%B3%E5%8F%B0') }],
-        })
+      if (!process.env.DEV_BUILD && lastCheckUpdate && isExtTainted) {
+        const diff = Math.floor((today - lastCheckUpdate) / 24 / 60 / 60 / 1000)
+        if (diff > 0 && diff % 7 === 0) {
+          browser.notifications.create('update', {
+            type: 'basic',
+            iconUrl: browser.runtime.getURL(`static/icon-128.png`),
+            title: decodeURI('%E6%B2%99%E6%8B%89%E6%9F%A5%E8%AF%8D'),
+            message: decodeURI('%E6%AD%A4%E3%80%8C%E6%B2%99%E6%8B%89%E6%9F%A5%E8%AF%8D%E3%80%8D%E6%89%A9%E5%B1%95%E5%B7%B2%E8%A2%AB%E4%BA%8C%E6%AC%A1%E6%89%93%E5%8C%85%EF%BC%8C%E8%AF%B7%E5%9C%A8%E5%AE%98%E6%96%B9%E5%BB%BA%E8%AE%AE%E7%9A%84%E5%B9%B3%E5%8F%B0%E5%AE%89%E8%A3%85%E3%80%82'),
+            buttons: [{ title: decodeURI('%E6%9F%A5%E7%9C%8B%E5%8F%AF%E9%9D%A0%E7%9A%84%E5%B9%B3%E5%8F%B0') }],
+          })
+        }
       }
     })
 
