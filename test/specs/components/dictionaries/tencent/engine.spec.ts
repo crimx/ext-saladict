d233472e5a7775134adc56d53c5d2fe52ff94520
CRIMX <straybugs@gmail.com>
2019-05-24 23:23:18 +0800

    test(dicts): fix tencent origin

--- test/specs/components/dictionaries/tencent/engine.spec.ts
+++ test/specs/components/dictionaries/tencent/engine.spec.ts
@@ -2,9 +2,21 @@ import { retry } from '../helpers'
 import { search } from '@/components/dictionaries/tencent/engine'
 import { getDefaultConfig } from '@/app-config'
 import { getDefaultProfile } from '@/app-config/profiles'
-import { isContainEnglish, isContainJapanese, isContainChinese } from '@/_helpers/lang-check'
+import { isContainEnglish, isContainChinese } from '@/_helpers/lang-check'
 
 describe('Dict/Tencent/engine', () => {
+  var fetch = window.fetch
+  beforeAll(() => {
+    fetch = window.fetch
+    window.fetch = (input: string | Request, init: RequestInit = {}) => fetch(
+      input, { ...init, headers: { ...(init.headers || {}), Origin: 'https://fanyi.qq.com' } }
+    )
+  })
+
+  afterAll(() => {
+    window.fetch = fetch
+  })
+
   beforeEach(() => {
     browser.storage.local.get.callsFake(() => Promise.resolve({}))
     browser.storage.local.set.callsFake(() => Promise.resolve())
@@ -19,7 +31,7 @@ describe('Dict/Tencent/engine', () => {
             expect(searchResult.result.trans.text).toMatch(/love/i)
             expect(searchResult.audio).toBeUndefined()
             expect(searchResult.result.id).toBe('tencent')
-            expect(searchResult.result.sl).toBe('auto')
+            expect(searchResult.result.sl).toBe('zh')
             expect(searchResult.result.tl).toBe('en')
             expect(isContainChinese(searchResult.result.searchText.text)).toBeTruthy()
             expect(typeof searchResult.result.trans.audio).toBe('string')
