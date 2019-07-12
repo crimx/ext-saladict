dd56c3632ae53b1603deec74f2d0ef5048f3a214
CRIMX <straybugs@gmail.com>
2019-07-12 15:28:22 +0800

    refactor: en locales

---
+++ src/_locales/en/popup.ts
@@ -0,0 +1,13 @@
+import { locale as _locale } from '../zh-CN/popup'
+
+export const locale: typeof _locale = {
+  app_active_title: 'Enable Inline Translator',
+  app_temp_active_title: 'Temporary disabled to the page',
+  instant_capture_pinned: ' (pinned) ',
+  instant_capture_title: 'Enable Instant Capture',
+  notebook_added: 'Added',
+  notebook_empty: 'No selection found on the current page',
+  notebook_error: 'Cannot add selected text to Notebook',
+  page_no_response: 'Page no response',
+  qrcode_title: 'Qrcode of the page'
+}
