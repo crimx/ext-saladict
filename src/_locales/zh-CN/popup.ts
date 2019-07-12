053d77b2930af3e0ba67d0dc3e686d0dbebcbe50
CRIMX <straybugs@gmail.com>
2019-07-12 15:28:03 +0800

    fix: locale format

--- src/_locales/zh-CN/popup.ts
+++ src/_locales/zh-CN/popup.ts
@@ -1,47 +1,11 @@
 export const locale = {
-  app_active_title: {
-    en: 'Enable Inline Translator',
-    zh_CN: '启用划词',
-    zh_TW: '啟用滑鼠選字'
-  },
-  app_temp_active_title: {
-    en: 'Temporary disabled to the page',
-    zh_CN: '对当前页暂时关闭划词',
-    zh_TW: '對目前頁面暫時關閉滑鼠選字'
-  },
-  instant_capture_pinned: {
-    en: ' (pinned) ',
-    zh_CN: '（钉住）',
-    zh_TW: '（釘選）'
-  },
-  instant_capture_title: {
-    en: 'Enable Instant Capture',
-    zh_CN: '开启鼠标悬浮取词',
-    zh_TW: '啟用滑鼠懸浮取詞'
-  },
-  notebook_added: {
-    en: 'Added',
-    zh_CN: '已添加',
-    zh_TW: '已新增'
-  },
-  notebook_empty: {
-    en: 'No selection found on the current page',
-    zh_CN: '当前页面没有发现选词',
-    zh_TW: '目前頁面沒有發現選詞'
-  },
-  notebook_error: {
-    en: 'Cannot add selected text to Notebook',
-    zh_CN: '无法添加选词到生词本',
-    zh_TW: '無法新增選詞到生字本'
-  },
-  page_no_response: {
-    en: 'Page no response',
-    zh_CN: '页面无响应',
-    zh_TW: '頁面無回應'
-  },
-  qrcode_title: {
-    en: 'Qrcode of the page',
-    zh_CN: '当前页面二维码',
-    zh_TW: '目前頁面二維條碼'
-  }
+  app_active_title: '启用划词',
+  app_temp_active_title: '对当前页暂时关闭划词',
+  instant_capture_pinned: '（钉住）',
+  instant_capture_title: '开启鼠标悬浮取词',
+  notebook_added: '已添加',
+  notebook_empty: '当前页面没有发现选词',
+  notebook_error: '无法添加选词到生词本',
+  page_no_response: '页面无响应',
+  qrcode_title: '当前页面二维码'
 }
