3e70b084c95aa240904f18c94252f000e498856a
crimx <straybugs@gmail.com>
2019-07-26 19:32:29 +0800

    refactor: split key event

--- src/content/components/MenuBar/FloatBox.tsx
+++ src/content/components/MenuBar/FloatBox.tsx
@@ -8,7 +8,12 @@ interface FloatBoxPropsBase {
   onBlur: (e: React.FocusEvent<HTMLButtonElement>) => any
   /** When a item is selected */
   onSelect: (key: string) => any
-  onClose: () => any
+  /** When ArrowUp key if pressed on the first item */
+  onArrowUpFirst?: (container: HTMLDivElement) => any
+  /** When ArrowDown key if pressed on the last item */
+  onArrowDownLast?: (container: HTMLDivElement) => any
+  /** When the panel is about to close */
+  onClose?: (container: HTMLDivElement) => any
 }
 
 interface FloatBoxPropsWithList extends FloatBoxPropsBase {
@@ -26,57 +31,66 @@ export type FloatBoxProps = FloatBoxPropsWithList | FloatBoxPropsLoading
  * A white box
  */
 export const FloatBox = React.forwardRef(
-  (props: FloatBoxProps, ref: React.Ref<HTMLDivElement>) => (
-    <div className="menuBar-FloatBoxContainer">
-      <AnimateHeight height={props.isLoading ? 32 : 'auto'}>
-        {props.isLoading ? (
-          <div className="lds-ellipsis">
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-          </div>
-        ) : (
-          <div ref={ref} className="menuBar-FloatBox">
-            {props.list.map(item => (
-              <button
-                key={item.key}
-                className="menuBar-FloatBoxBtn"
-                onFocus={props.onFocus}
-                onBlur={props.onBlur}
-                onClick={e => props.onSelect(e.currentTarget.dataset.key!)}
-                onKeyDown={e => {
-                  if (e.key === 'ArrowDown') {
-                    e.preventDefault()
-                    e.stopPropagation()
-                    const $nextLi = e.currentTarget.nextSibling
-                    if ($nextLi) {
-                      ;($nextLi as HTMLButtonElement).focus()
-                    }
-                  } else if (e.key === 'ArrowUp') {
-                    e.preventDefault()
-                    e.stopPropagation()
-                    const $prevLi = e.currentTarget.previousSibling
-                    if ($prevLi) {
-                      ;($prevLi as HTMLButtonElement).focus()
-                    } else {
-                      props.onClose()
-                    }
-                  } else if (e.key === 'Escape') {
-                    // prevent the dict panel being closed
-                    e.preventDefault()
-                    e.stopPropagation()
-                    props.onClose()
-                  }
-                }}
-                data-key={item.key}
-              >
-                {item.content}
-              </button>
-            ))}
-          </div>
-        )}
-      </AnimateHeight>
-    </div>
-  )
+  (props: FloatBoxProps, containerRef: React.Ref<HTMLDivElement>) => {
+    return (
+      <div className="menuBar-FloatBoxContainer">
+        <AnimateHeight height={props.isLoading ? 32 : 'auto'}>
+          {props.isLoading ? (
+            <div className="lds-ellipsis">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+          ) : (
+              <div ref={containerRef} className="menuBar-FloatBox">
+                {props.list.map(item => (
+                  <button
+                    key={item.key}
+                    className="menuBar-FloatBoxBtn"
+                    onFocus={props.onFocus}
+                    onBlur={props.onBlur}
+                    onClick={e => props.onSelect(e.currentTarget.dataset.key!)}
+                    onKeyDown={e => {
+                      if (e.key === 'ArrowDown') {
+                        e.preventDefault()
+                        e.stopPropagation()
+                        const $nextLi = e.currentTarget.nextSibling
+                        if ($nextLi) {
+                          ; ($nextLi as HTMLButtonElement).focus()
+                        } else if (props.onArrowDownLast) {
+                          props.onArrowDownLast(e.currentTarget
+                            .parentElement as HTMLDivElement)
+                        }
+                      } else if (e.key === 'ArrowUp') {
+                        e.preventDefault()
+                        e.stopPropagation()
+                        const $prevLi = e.currentTarget.previousSibling
+                        if ($prevLi) {
+                          ; ($prevLi as HTMLButtonElement).focus()
+                        } else if (props.onArrowUpFirst) {
+                          props.onArrowUpFirst(e.currentTarget
+                            .parentElement as HTMLDivElement)
+                        }
+                      } else if (e.key === 'Escape') {
+                        // prevent the dict panel being closed
+                        e.preventDefault()
+                        e.stopPropagation()
+                        if (props.onClose) {
+                          props.onClose(e.currentTarget
+                            .parentElement as HTMLDivElement)
+                        }
+                      }
+                    }}
+                    data-key={item.key}
+                  >
+                    {item.content}
+                  </button>
+                ))}
+              </div>
+            )}
+        </AnimateHeight>
+      </div>
+    )
+  }
 )
