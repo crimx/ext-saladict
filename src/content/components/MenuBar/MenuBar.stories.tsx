aa7a808dbdf9c9e83e8fc4764af507f812eec1b3
crimx <straybugs@gmail.com>
2019-07-27 22:22:39 +0800

    refactor: menu bar

---
+++ src/content/components/MenuBar/MenuBar.stories.tsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { withInfo } from '@storybook/addon-info'
+import { action } from '@storybook/addon-actions'
+import {
+  withKnobs,
+  select,
+  text,
+  number,
+  boolean
+} from '@storybook/addon-knobs'
+import {
+  withLocalStyle,
+  withi18nNS,
+  withSaladictPanel
+} from '@/_helpers/storybook'
+import { newWord } from '@/_helpers/record-manager'
+import { MenuBar } from './MenuBar'
+
+storiesOf('Content Scripts|Menubar', module)
+  .addDecorator(
+    withInfo({
+      inline: true,
+      header: false
+    })
+  )
+  .addDecorator(withKnobs)
+  .addDecorator(withSaladictPanel)
+  .addDecorator(withLocalStyle(require('./MenuBar.scss')))
+  .addDecorator(withLocalStyle(require('@/_sass_global/_reset.scss')))
+  .addDecorator(withi18nNS('content'))
+  .add('MenuBar', () => {
+    const histories = Array.from(Array(5)).map((_, i) =>
+      newWord({
+        date: Date.now(),
+        text: 'text' + (i + 1)
+      })
+    )
+
+    const profiles = Array.from(Array(5)).map((_, i) => ({
+      id: `profile${i + 1}`,
+      name: `Profile${i + 1}`
+    }))
+
+    const profilesOption = profiles.reduce((o, p) => {
+      o[p.name] = p.id
+      return o
+    }, {})
+
+    return (
+      <MenuBar
+        text={text('Search Text', 'text')}
+        updateText={action('Update Text')}
+        searchText={action('Search Text')}
+        isInNotebook={boolean('Is In Notebook', false)}
+        addToNoteBook={action('Add to Notebook')}
+        isFocusInputOnMount={true}
+        enableSuggest={boolean('Enable Suggest', true)}
+        histories={histories}
+        historyIndex={number('History Index', 0)}
+        updateHistoryIndex={action('Update History Index')}
+        isPinned={boolean('Is Pinned', false)}
+        togglePin={action('Toggle Pin')}
+        onClose={action('Close Panel')}
+        profiles={profiles}
+        activeProfileId={select(
+          'Active Profile',
+          profilesOption,
+          profiles[0].id
+        )}
+        onDragAreaMouseDown={action('Darg Area Mousedown')}
+        onDragAreaTouchStart={action('Darg Area Touchstart')}
+      />
+    )
+  })
---
+++ src/content/components/MenuBar/MenuBar.tsx
@@ -0,0 +1,96 @@
+import React from 'react'
+import { Word } from '@/_helpers/record-manager'
+import { useTranslate } from '@/_helpers/i18n'
+import {
+  HistoryBackBtn,
+  HistoryNextBtn,
+  SearchBtn,
+  FavBtn,
+  HistoryBtn,
+  PinBtn,
+  CloseBtn
+} from './MenubarBtns'
+import { SearchBox, SearchBoxProps } from './SearchBox'
+import { Profiles, ProfilesProps } from './Profiles'
+import { message } from '@/_helpers/browser-api'
+
+const isSaladictPopupPage = !!window.__SALADICT_POPUP_PAGE__
+const isSaladictQuickSearchPage = !!window.__SALADICT_QUICK_SEARCH_PAGE__
+const isStandalonePage = isSaladictPopupPage || isSaladictQuickSearchPage
+
+export interface MenuBarProps {
+  text: string
+  updateText: SearchBoxProps['onInput']
+  searchText: (text: string) => any
+
+  /** is in Notebook */
+  isInNotebook: boolean
+  addToNoteBook: () => any
+
+  isFocusInputOnMount: boolean
+  enableSuggest: boolean
+
+  histories: Word[]
+  historyIndex: number
+  updateHistoryIndex: (index: number) => any
+
+  profiles: ProfilesProps['profiles']
+  activeProfileId: ProfilesProps['activeProfileId']
+
+  isPinned: boolean
+  togglePin: () => any
+
+  onClose: () => any
+
+  onDragAreaMouseDown: (e: React.MouseEvent<HTMLDivElement>) => any
+  onDragAreaTouchStart: (e: React.TouchEvent<HTMLDivElement>) => any
+}
+
+export const MenuBar: React.FC<MenuBarProps> = props => {
+  const { t } = useTranslate(['content', 'common'])
+  return (
+    <header className="menuBar">
+      <HistoryBackBtn
+        t={t}
+        disabled={props.historyIndex <= 0}
+        onClick={() => props.updateHistoryIndex(props.historyIndex - 1)}
+      />
+      <HistoryNextBtn
+        t={t}
+        disabled={props.historyIndex >= props.histories.length - 1}
+        onClick={() => props.updateHistoryIndex(props.historyIndex + 1)}
+      />
+      <SearchBox
+        t={t}
+        text={props.text}
+        isFocusOnMount={props.isFocusInputOnMount}
+        enableSuggest={props.enableSuggest}
+        onInput={props.updateText}
+        onSearch={props.searchText}
+      />
+      <SearchBtn t={t} onClick={() => props.searchText(props.text)} />
+      <div
+        className={`menuBar-DragArea${isStandalonePage ? '' : ' isActive'}`}
+        onMouseDown={props.onDragAreaMouseDown}
+        onTouchStart={props.onDragAreaTouchStart}
+      />
+      <Profiles
+        t={t}
+        profiles={props.profiles}
+        activeProfileId={props.activeProfileId}
+      />
+      <FavBtn t={t} isFav={props.isInNotebook} onClick={props.addToNoteBook} />
+      <HistoryBtn
+        t={t}
+        onClick={() =>
+          message.send({
+            type: 'OPEN_URL',
+            payload: { url: 'history.html', self: true }
+          })
+        }
+      />
+      <PinBtn t={t} isPinned={props.isPinned} onClick={props.togglePin} />
+      <CloseBtn t={t} onClick={props.onClose} />
+    </header>
+  )
+}
