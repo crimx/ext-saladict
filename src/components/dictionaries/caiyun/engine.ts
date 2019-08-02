a07ea04699244477ae3a71a3c5dfa2574f61b9da
crimx <straybugs@gmail.com>
2019-08-02 16:17:22 +0800

    refactor(dicts): caiyun

--- src/components/dictionaries/caiyun/engine.ts
+++ src/components/dictionaries/caiyun/engine.ts
@@ -1,15 +1,16 @@
+import axios from 'axios'
 import {
   handleNoResult,
   MachineTranslateResult,
   SearchFunction,
   MachineTranslatePayload,
   GetSrcPageFunction,
+  DictSearchResult
 } from '../helpers'
-import { DictSearchResult } from '@/typings/server'
 import { isContainChinese, isContainJapanese } from '@/_helpers/lang-check'
 import { storage } from '@/_helpers/browser-api'
 
-export const getSrcPage: GetSrcPageFunction = (text, config, profile) => {
+export const getSrcPage: GetSrcPageFunction = () => {
   return 'https://fanyi.caiyunapp.com/'
 }
 
@@ -24,24 +25,25 @@ export type CaiyunResult = MachineTranslateResult<'caiyun'>
 
 type CaiyunSearchResult = DictSearchResult<CaiyunResult>
 
-const langcodes: ReadonlyArray<string> = [
-  'zh', 'ja', 'en',
-]
+const langcodes: ReadonlyArray<string> = ['zh', 'ja', 'en']
 
-export const search: SearchFunction<CaiyunSearchResult, MachineTranslatePayload> = async (
-  text, config, profile, payload
-) => {
+export const search: SearchFunction<
+  CaiyunResult,
+  MachineTranslatePayload
+> = async (text, config, profile, payload) => {
   const options = profile.dicts.all.caiyun.options
 
-  let sl: string = payload.sl || (
-    isContainJapanese(text) ? 'ja' : isContainChinese(text) ? 'zh' : 'en'
-  )
+  let sl: string =
+    payload.sl ||
+    (isContainJapanese(text) ? 'ja' : isContainChinese(text) ? 'zh' : 'en')
 
-  let tl: string = payload.tl || (
-    options.tl === 'default'
-      ? config.langCode.startsWith('zh') ? 'zh' : 'en'
-      : options.tl
-  )
+  let tl: string =
+    payload.tl ||
+    (options.tl === 'default'
+      ? config.langCode.startsWith('zh')
+        ? 'zh'
+        : 'en'
+      : options.tl)
 
   if (sl === tl) {
     if (isContainJapanese(text)) {
@@ -66,43 +68,49 @@ export const search: SearchFunction<CaiyunSearchResult, MachineTranslatePayload>
     text = text.replace(/\n+/g, ' ')
   }
 
-  return fetch(
-    'https://api.interpreter.caiyunai.com/v1/translator',
-    {
-      headers: {
-        'Accept': 'application/json, text/plain, */*',
-        'Content-Type': 'application/json;charset=UTF-8',
-        'DNT': '1',
-        'Origin': 'https://fanyi.caiyunapp.com',
-        'Referer': 'https://fanyi.caiyunapp.com/',
-        'X-Authorization': await getToken(),
-      },
-      credentials: 'omit',
-      method: 'POST',
-      body: JSON.stringify({
-        media: 'text',
-        os_type: 'web',
-        request_id: 'web_fanyi',
-        source: text,
-        trans_type: `${sl}2${tl}`,
+  return (
+    axios
+      .post('https://api.interpreter.caiyunai.com/v1/translator', {
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json;charset=UTF-8',
+          DNT: '1',
+          Origin: 'https://fanyi.caiyunapp.com',
+          Referer: 'https://fanyi.caiyunapp.com/',
+          'X-Authorization': await getToken()
+        },
+        withCredentials: false,
+        responseType: 'json',
+        data: JSON.stringify({
+          media: 'text',
+          os_type: 'web',
+          request_id: 'web_fanyi',
+          source: text,
+          trans_type: `${sl}2${tl}`
+        })
       })
-    }
+      .then(({ data }) => handleJSON(data, sl, tl, text))
+      // return empty result so that user can still toggle language
+      .catch(
+        (): CaiyunSearchResult => ({
+          result: {
+            id: 'caiyun',
+            sl,
+            tl,
+            langcodes,
+            searchText: { text: '' },
+            trans: { text: '' }
+          }
+        })
+      )
   )
-  .then(r => r.json())
-  .then(json => handleJSON(json, sl, tl, text))
-  // return empty result so that user can still toggle language
-  .catch((): CaiyunSearchResult => ({
-    result: {
-      id: 'caiyun',
-      sl, tl, langcodes,
-      searchText: { text: '' },
-      trans: { text: '' }
-    }
-  }))
 }
 
-function handleJSON (
-  json: any, sl: string, tl: string, text: string
+function handleJSON(
+  json: any,
+  sl: string,
+  tl: string,
+  text: string
 ): CaiyunSearchResult | Promise<CaiyunSearchResult> {
   const trans: string | undefined = json && json.target
   if (!trans) {
@@ -112,39 +120,63 @@ function handleJSON (
   return {
     result: {
       id: 'caiyun',
-      sl, tl, langcodes,
+      sl,
+      tl,
+      langcodes,
       trans: {
         text: trans,
-        audio: `http://tts.baidu.com/text2audio?lan=zh&ie=UTF-8&spd=5&text=${encodeURIComponent(trans)}`
+        audio: `http://tts.baidu.com/text2audio?lan=zh&ie=UTF-8&spd=5&text=${encodeURIComponent(
+          trans
+        )}`
       },
       searchText: {
         text,
-        audio: `http://tts.baidu.com/text2audio?lan=zh&ie=UTF-8&spd=5&text=${encodeURIComponent(text)}`
+        audio: `http://tts.baidu.com/text2audio?lan=zh&ie=UTF-8&spd=5&text=${encodeURIComponent(
+          text
+        )}`
       }
     },
     audio: {
-      us: `http://tts.baidu.com/text2audio?lan=zh&ie=UTF-8&spd=5&text=${encodeURIComponent(trans)}`
+      us: `http://tts.baidu.com/text2audio?lan=zh&ie=UTF-8&spd=5&text=${encodeURIComponent(
+        trans
+      )}`
     }
   }
 }
 
-async function getToken (): Promise<string> {
-  let { dict_caiyun } = await storage.local.get<{'dict_caiyun': CaiyunStorage}>('dict_caiyun')
-  if (!dict_caiyun || (Date.now() - dict_caiyun.tokenDate > 15 * 60000)) {
+async function getToken(): Promise<string> {
+  let { dict_caiyun } = await storage.local.get<{ dict_caiyun: CaiyunStorage }>(
+    'dict_caiyun'
+  )
+  if (!dict_caiyun || Date.now() - dict_caiyun.tokenDate > 15 * 60000) {
     let token = 'token:3975l6lr5pcbvidl6jl2'
     try {
-      const homepage = await fetch('https://fanyi.caiyunapp.com', { credentials: 'omit' })
-        .then(r => r.text())
+      const { data: homepage } = await axios.get<string>(
+        'https://fanyi.caiyunapp.com',
+        {
+          withCredentials: false,
+          responseType: 'text'
+        }
+      )
 
-      const appjsPath = (homepage.match(/\/static\/js\/app\.\w+\.js/) || [''])[0]
+      const appjsPath = (homepage.match(/\/static\/js\/app\.\w+\.js/) || [
+        ''
+      ])[0]
       if (appjsPath) {
-        const appjs = await fetch('https://fanyi.caiyunapp.com' + appjsPath).then(r => r.text())
+        const { data: appjs } = await axios.get<string>(
+          'https://fanyi.caiyunapp.com' + appjsPath,
+          {
+            responseType: 'text'
+          }
+        )
         const matchRes = appjs.match(/token:\w+/)
         if (matchRes) {
           token = matchRes[0]
         }
       }
-    } catch (e) {/* nothing */}
+    } catch (e) {
+      /* nothing */
+    }
     dict_caiyun = {
       token,
       tokenDate: Date.now()
