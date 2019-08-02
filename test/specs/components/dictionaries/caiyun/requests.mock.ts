08e28bd0b8472a3e8d80057451e9cf92f058fb84
crimx <straybugs@gmail.com>
2019-08-02 16:17:22 +0800

    refactor(dicts): caiyun

---
+++ test/specs/components/dictionaries/caiyun/requests.mock.ts
@@ -0,0 +1,26 @@
+import { MockRequest } from '@/components/dictionaries/helpers'
+
+export const mockSearchTexts = ['I love you']
+
+export const mockRequest: MockRequest = mock => {
+  mock
+    .onGet('https://fanyi.caiyunapp.com')
+    .reply(
+      200,
+      `<script type=text/javascript src=/static/js/app.777648be66595ff35e36.js> </script>`
+    )
+
+  mock
+    .onGet(/js\/app/)
+    .reply(
+      200,
+      `(t.headers['X-Authorization'] = 'token:cy4fgbil24jucmh8jfr5'), t)`
+    )
+
+  mock.onPost(/interpreter/).reply(200, {
+    confidence: 0.8,
+    target: '\u4eca\u591c\u6708\u8272\u5f88\u7f8e',
+    isdict: 1,
+    rc: 0
+  })
+}
