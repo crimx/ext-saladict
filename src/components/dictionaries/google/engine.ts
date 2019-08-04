4c686eea3298e32a54bd8e0523a8c2d645b350c5
crimx <straybugs@gmail.com>
2019-08-04 14:52:11 +0800

    refactor(dicts): google

--- src/components/dictionaries/google/engine.ts
+++ src/components/dictionaries/google/engine.ts
@@ -5,16 +5,22 @@ import {
   SearchFunction,
   MachineTranslatePayload,
   GetSrcPageFunction,
+  DictSearchResult
 } from '../helpers'
-import { DictSearchResult } from '@/typings/server'
-import { isContainChinese, isContainJapanese, isContainKorean } from '@/_helpers/lang-check'
+import {
+  isContainChinese,
+  isContainJapanese,
+  isContainKorean
+} from '@/_helpers/lang-check'
 import { first } from '@/_helpers/promise-more'
+import axios from 'axios'
 
 export const getSrcPage: GetSrcPageFunction = (text, config, profile) => {
   const domain = profile.dicts.all.google.options.cnfirst ? 'cn' : 'com'
-  const lang = profile.dicts.all.google.options.tl === 'default'
-    ? config.langCode
-    : profile.dicts.all.google.options.tl
+  const lang =
+    profile.dicts.all.google.options.tl === 'default'
+      ? config.langCode
+      : profile.dicts.all.google.options.tl
 
   return `https://translate.google.${domain}/#auto/${lang}/${text}`
 }
@@ -22,8 +28,8 @@ export const getSrcPage: GetSrcPageFunction = (text, config, profile) => {
 export type GoogleResult = MachineTranslateResult<'google'>
 
 interface GoogleRawResult {
-  json: string
-  base?: string
+  json: string | object
+  base: string
   sl: string
   tl: string
   tk1?: number
@@ -33,6 +39,7 @@ interface GoogleRawResult {
 
 type GoogleSearchResult = DictSearchResult<GoogleResult>
 
+// prettier-ignore
 const langcodes: ReadonlyArray<string> = [
   'zh-CN', 'zh-TW', 'en',
   'af', 'am', 'ar', 'az', 'be', 'bg', 'bn', 'bs', 'ca', 'ceb', 'co', 'cs', 'cy', 'da', 'de',
@@ -44,92 +51,155 @@ const langcodes: ReadonlyArray<string> = [
   'th', 'tl', 'tr', 'uk', 'ur', 'uz', 'vi', 'xh', 'yi', 'yo', 'zu',
 ]
 
-export const search: SearchFunction<GoogleSearchResult, MachineTranslatePayload> = (
-  text, config, profile, payload
+export const search: SearchFunction<GoogleResult, MachineTranslatePayload> = (
+  text,
+  config,
+  profile,
+  payload
 ) => {
   const options = profile.dicts.all.google.options
 
   const sl: string = payload.sl || 'auto'
-  const tl: string = payload.tl || (
-    options.tl === 'default'
+  const tl: string =
+    payload.tl ||
+    (options.tl === 'default'
       ? config.langCode === 'en'
         ? 'en'
-        : !isContainChinese(text) || isContainJapanese(text) || isContainKorean(text)
-          ? config.langCode === 'zh-TW' ? 'zh-TW' : 'zh-CN'
-          : 'en'
-      : options.tl
-  )
+        : !isContainChinese(text) ||
+          isContainJapanese(text) ||
+          isContainKorean(text)
+        ? config.langCode === 'zh-TW'
+          ? 'zh-TW'
+          : 'zh-CN'
+        : 'en'
+      : options.tl)
 
   if (payload.isPDF && !options.pdfNewline) {
     text = text.replace(/\n+/g, ' ')
   }
 
-  return first([
-    fetchWithToken('https://translate.google.com', sl, tl, text),
-    fetchWithToken('https://translate.google.cn', sl, tl, text),
-  ])
-  .catch(() => fetchWithoutToken(sl, tl, text))
-  .then(handleText)
-  // return empty result so that user can still toggle language
-  .catch((): GoogleSearchResult => ({
-    result: {
-      id: 'google',
-      sl, tl, langcodes,
-      searchText: { text: '' },
-      trans: { text: '' }
-    }
-  }))
+  return (
+    first([
+      fetchWithToken('https://translate.google.com', sl, tl, text),
+      fetchWithToken('https://translate.google.cn', sl, tl, text)
+    ])
+      .catch(() => fetchWithoutToken(sl, tl, text))
+      .then(handleText)
+      // return empty result so that user can still toggle language
+      .catch(
+        (): GoogleSearchResult => ({
+          result: {
+            id: 'google',
+            sl,
+            tl,
+            langcodes,
+            searchText: { text: '' },
+            trans: { text: '' }
+          }
+        })
+      )
+  )
 }
 
-function fetchWithToken (base: string, sl: string, tl: string, text: string): Promise<GoogleRawResult> {
-  return fetch(base)
-    .then(r => r.ok ? r.text() : handleNetWorkError())
-    .then<GoogleRawResult>(body => {
-      let tk = ''
-      let tk1 = 0
-      let tk2 = 0
-
-      // eval version
-      let tkk = (body.match(/TKK=(.*?)\(\)\)'\);/i) || [''])[0]
-        .replace(/\\x([0-9A-Fa-f]{2})/g, '') // remove hex chars
-        .match(/[+-]?\d+/g)
-      if (tkk) {
-        tk1 = Number(tkk[2])
-        tk2 = Number(tkk[0]) + Number(tkk[1])
-        tk = getTK(text, tk1, tk2)
-      }
-
-      // direct number
-      if (!tk) {
-        tkk = body.match(/TKK[=:]['"](\d+?)\.(\d+?)['"]/i)
-        if (tkk) {
-          tk1 = Number(tkk[1])
-          tk2 = Number(tkk[2])
-          tk = getTK(text, tk1, tk2)
-        }
-      }
-
-      if (tk) {
-        const encodedText = encodeURIComponent(text)
-        return fetch(`${base}/translate_a/single?client=t&sl=${sl}&tl=${tl}&q=${encodedText}&tk=${tk}&hl=en&dt=at&dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t&ie=UTF-8&oe=UTF-8&otf=1&ssel=0&tsel=0&kc=5`)
-          .then(r => r.text())
-          .then(json => ({ json, base, sl, tl, tk1, tk2, text }))
-      }
-
-      return handleNoResult()
+async function fetchWithToken(
+  base: string,
+  sl: string,
+  tl: string,
+  text: string
+): Promise<GoogleRawResult> {
+  const { data: homepage } = await axios
+    .get<string>(base, {
+      responseType: 'text'
     })
+    .catch(handleNetWorkError)
+
+  if (!homepage) {
+    handleNetWorkError()
+  }
+
+  let tk = ''
+  let tk1 = 0
+  let tk2 = 0
+
+  // eval version
+  let tkk = (homepage.match(/TKK=(.*?)\(\)\)'\);/i) || [''])[0]
+    .replace(/\\x([0-9A-Fa-f]{2})/g, '') // remove hex chars
+    .match(/[+-]?\d+/g)
+  if (tkk) {
+    tk1 = Number(tkk[2])
+    tk2 = Number(tkk[0]) + Number(tkk[1])
+    tk = getTK(text, tk1, tk2)
+  }
+
+  // direct number
+  if (!tk) {
+    tkk = homepage.match(/TKK[=:]['"](\d+?)\.(\d+?)['"]/i)
+    if (tkk) {
+      tk1 = Number(tkk[1])
+      tk2 = Number(tkk[2])
+      tk = getTK(text, tk1, tk2)
+    }
+  }
+
+  if (tk) {
+    const params = new URLSearchParams(
+      `?client=t&hl=en&dt=at&dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t&ie=UTF-8&oe=UTF-8&otf=1&ssel=0&tsel=0&kc=5`
+    )
+    params.append('sl', sl)
+    params.append('tl', tl)
+    params.append('tk', tk)
+    params.append('q', text)
+
+    const { data: json } = await axios.get<object | string>(
+      `${base}/translate_a/single`,
+      {
+        params,
+        responseType: 'text'
+      }
+    )
+
+    return { json, base, sl, tl, tk1, tk2, text }
+  }
+
+  return handleNoResult()
 }
 
-function fetchWithoutToken (sl: string, tl: string, text: string): Promise<GoogleRawResult> {
-  return fetch(`https://translate.googleapis.com/translate_a/single?client=gtx&sl=${sl}&tl=${tl}&dt=t&q=${encodeURIComponent(text)}`)
-    .then(r => r.ok ? r.text() : handleNetWorkError())
-    .then(json => ({ json, sl, tl, text }))
+async function fetchWithoutToken(
+  sl: string,
+  tl: string,
+  text: string
+): Promise<GoogleRawResult> {
+  const { data: json } = await axios
+    .get<string | object>(
+      'https://translate.googleapis.com/translate_a/single',
+      {
+        params: new URLSearchParams({
+          client: 'gtx',
+          dt: 't',
+          sl,
+          tl,
+          q: text
+        }),
+        responseType: 'text'
+      }
+    )
+    .catch(handleNetWorkError)
+
+  return { json, base: 'https://translate.google.cn', sl, tl, text }
 }
 
-function handleText (
-  { json, base, sl, tl, tk1, tk2, text }: GoogleRawResult
-): GoogleSearchResult | Promise<GoogleSearchResult> {
-  const data = JSON.parse(json.replace(/,+/g, ','))
+function handleText({
+  json,
+  base,
+  sl,
+  tl,
+  tk1,
+  tk2,
+  text
+}: GoogleRawResult): GoogleSearchResult | Promise<GoogleSearchResult> {
+  // axios bug https://github.com/axios/axios/issues/907
+  const data =
+    typeof json === 'string' ? JSON.parse(json.replace(/,+/g, ',')) : json
 
   if (!data[0] || data[0].length <= 0) {
     return handleNoResult()
@@ -141,22 +211,21 @@ function handleText (
     .join(' ')
 
   if (transText.length > 0) {
-    const transAudio = tk1 || tk2
-      ? `${base}/translate_tts?ie=UTF-8&q=${encodeURIComponent(transText)}&tl=${tl}&total=1&idx=0&tk=${getTK(transText, tk1, tk2)}&client=t`
-      : ''
+    const transAudio =
+      tk1 && tk2 ? getAudioLink(base, transText, tl, tk1, tk2) : ''
     return {
       result: {
         id: 'google',
-        sl, tl, langcodes,
+        sl,
+        tl,
+        langcodes,
         trans: {
           text: transText,
           audio: transAudio
         },
         searchText: {
           text,
-          audio: tk1 || tk2
-            ? `${base}/translate_tts?ie=UTF-8&q=${encodeURIComponent(text)}&tl=${data[2]}&total=1&idx=0&tk=${getTK(text, tk1, tk2)}&client=t`
-            : ''
+          audio: tk1 && tk2 ? getAudioLink(base, text, data[2], tk1, tk2) : ''
         }
       },
       audio: {
@@ -168,8 +237,22 @@ function handleText (
   return handleNoResult()
 }
 
-/* tslint:disable */
+function getAudioLink(
+  base: string,
+  text: string,
+  tl: string,
+  tk1: number,
+  tk2: number
+) {
+  const url = new URL(`${base}/translate_tts?ie=UTF-8&total=1&idx=0&client=t`)
+  url.searchParams.append('q', text)
+  url.searchParams.append('tl', tl)
+  url.searchParams.append('tk', getTK(text, tk1, tk2))
+  return url.toString()
+}
 
+/* tslint:disable */
+// prettier-ignore
 function getTK (a, b, c) {
   b = Number(b) || 0
   let e: any = []
@@ -194,6 +277,7 @@ function getTK (a, b, c) {
   return (a.toString() + '.' + (a ^ b))
 }
 
+// prettier-ignore
 function _magic (a, b) {
   for (var c = 0; c < b.length - 2; c += 3) {
     // @ts-ignore
