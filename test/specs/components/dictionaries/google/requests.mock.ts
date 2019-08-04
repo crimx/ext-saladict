f15749f76fe21fd4ee13369b2d456435a2dffb1e
crimx <straybugs@gmail.com>
2019-08-04 14:52:11 +0800

    refactor(dicts): google

---
+++ test/specs/components/dictionaries/google/requests.mock.ts
@@ -0,0 +1,15 @@
+import { MockRequest } from '@/components/dictionaries/helpers'
+
+export const mockSearchTexts = [
+  `It is what you read when you don't have to that determines what you will be when you can't help it.” \n― Oscar Wilde\nIt takes a long time to become an overnight success.`
+]
+
+export const mockRequest: MockRequest = mock => {
+  mock
+    .onGet(/translate_a/)
+    .reply(200, require('raw-loader!./response/f.txt').default)
+
+  mock
+    .onGet(/google/)
+    .reply(200, require('raw-loader!./response/homepage.html').default)
+}
