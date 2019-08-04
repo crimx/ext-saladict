d611ec6d5ef1b8c292f3f7be075453dbbff0a1b1
crimx <straybugs@gmail.com>
2019-08-04 18:58:51 +0800

    refactor(dicts): jukuu

---
+++ test/specs/components/dictionaries/jukuu/requests.mock.ts
@@ -0,0 +1,15 @@
+import { MockRequest } from '@/components/dictionaries/helpers'
+
+export const mockSearchTexts = ['love']
+
+export const mockRequest: MockRequest = mock => {
+  mock
+    .onGet(/jukuu/)
+    .reply(
+      200,
+      new DOMParser().parseFromString(
+        require(`raw-loader!./response/love.html`).default,
+        'text/html'
+      )
+    )
+}
