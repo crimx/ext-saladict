c9c90415223c49f01e2e053455dc1ac3fd091c20
crimx <straybugs@gmail.com>
2019-08-04 13:02:32 +0800

    refactor(dicts): eudic

--- src/components/dictionaries/eudic/engine.ts
+++ src/components/dictionaries/eudic/engine.ts
@@ -5,10 +5,10 @@ import {
   handleNetWorkError,
   SearchFunction,
   GetSrcPageFunction,
+  DictSearchResult
 } from '../helpers'
-import { DictSearchResult } from '@/typings/server'
 
-export const getSrcPage: GetSrcPageFunction = (text) => {
+export const getSrcPage: GetSrcPageFunction = text => {
   return `https://dict.eudic.net/dicts/en/${text}`
 }
 
@@ -23,27 +23,34 @@ export type EudicResult = EudicResultItem[]
 
 type EudicSearchResult = DictSearchResult<EudicResult>
 
-export const search: SearchFunction<EudicSearchResult> = (
-  text, config, profile, payload
+export const search: SearchFunction<EudicResult> = (
+  text,
+  config,
+  profile,
+  payload
 ) => {
-  text = encodeURIComponent(text.split(/\s+/).slice(0, 2).join(' '))
+  text = encodeURIComponent(
+    text
+      .split(/\s+/)
+      .slice(0, 2)
+      .join(' ')
+  )
   const options = profile.dicts.all.eudic.options
 
-  return fetchDirtyDOM(
-      'https://dict.eudic.net/dicts/en/' + text,
-      { credentials: 'omit' },
-    )
+  return fetchDirtyDOM('https://dict.eudic.net/dicts/en/' + text, {
+    withCredentials: false
+  })
     .catch(handleNetWorkError)
     .then(validator)
     .then(doc => handleDOM(doc, options))
 }
 
-function handleDOM (
+function handleDOM(
   doc: Document,
-  { resultnum }: { resultnum: number },
+  { resultnum }: { resultnum: number }
 ): EudicSearchResult | Promise<EudicSearchResult> {
   const result: EudicResult = []
-  const audio: { uk?: string, us?: string } = {}
+  const audio: { uk?: string; us?: string } = {}
 
   const $items = Array.from(doc.querySelectorAll('#lj_ting .lj_item'))
   for (let i = 0; i < $items.length && result.length < resultnum; i++) {
@@ -51,16 +58,23 @@ function handleDOM (
     const item: EudicResultItem = { chs: '', eng: '' }
 
     item.chs = getText($item, '.exp')
-    if (!item.chs) { continue }
+    if (!item.chs) {
+      continue
+    }
 
     item.eng = getText($item, '.line')
-    if (!item.eng) { continue }
+    if (!item.eng) {
+      continue
+    }
 
     item.channel = getText($item, '.channel_title')
 
     const audioID = $item.getAttribute('source')
     if (audioID) {
-      const mp3 = 'https://fs-gateway.eudic.net/store_main/sentencemp3/' + audioID + '.mp3'
+      const mp3 =
+        'https://fs-gateway.eudic.net/store_main/sentencemp3/' +
+        audioID +
+        '.mp3'
       item.mp3 = mp3
       if (!audio.us) {
         audio.us = mp3
@@ -78,23 +92,22 @@ function handleDOM (
   return handleNoResult()
 }
 
-function validator (doc: Document): Document | Promise<Document> {
+function validator(doc: Document): Document | Promise<Document> {
   if (doc.querySelector('#TingLiju')) {
     return doc
   }
 
   const status = doc.querySelector('#page-status') as HTMLInputElement
-  if (!status || !status.value) { return handleNoResult() }
+  if (!status || !status.value) {
+    return handleNoResult()
+  }
 
   const formData = new FormData()
   formData.append('status', status.value)
 
-  return fetchDirtyDOM(
-    'https://dict.eudic.net/Dicts/en/tab-detail/-12',
-    {
-      method: 'POST',
-      body: formData,
-      credentials: 'omit',
-    }
-  )
+  return fetchDirtyDOM('https://dict.eudic.net/Dicts/en/tab-detail/-12', {
+    method: 'POST',
+    data: formData,
+    withCredentials: false
+  })
 }
