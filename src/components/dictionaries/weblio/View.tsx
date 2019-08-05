3b511658321bfebb65d8c57574006368c47369ca
crimx <straybugs@gmail.com>
2019-08-05 22:30:19 +0800

    refactor(dicts): weblio

--- src/components/dictionaries/weblio/View.tsx
+++ src/components/dictionaries/weblio/View.tsx
@@ -1,18 +1,20 @@
-import React from 'react'
+import React, { FC } from 'react'
 import { WeblioResult } from './engine'
 import { ViewPorps } from '@/components/dictionaries/helpers'
+import EntryBox from '@/components/EntryBox'
 
-export default class DictWeblio extends React.PureComponent<ViewPorps<WeblioResult>> {
-  render () {
-    return (
-      <div className='dictWeblio-Container'>{
-        this.props.result.map(({ title, def }) => (
-          <div className='dictWeblio-Entry'>
-            <h2 className='dictWeblio-Title' dangerouslySetInnerHTML={{ __html: title }} />
-            <div dangerouslySetInnerHTML={{ __html: def }} />
-          </div>
-        ))
-      }</div>
-    )
-  }
-}
+export const DictWeblio: FC<ViewPorps<WeblioResult>> = ({ result }) => (
+  <div className="dictWeblio-Container">
+    {result.map(({ title, def }) => (
+      <EntryBox
+        key={title}
+        className="dictWeblio-Entry"
+        title={<span dangerouslySetInnerHTML={{ __html: title }} />}
+      >
+        <div dangerouslySetInnerHTML={{ __html: def }} />
+      </EntryBox>
+    ))}
+  </div>
+)
+
+export default DictWeblio
