e9e8c8862685b92c7f1c2f5a20af3258a43e2b61
crimx <straybugs@gmail.com>
2019-08-05 11:25:47 +0800

    refactor(dicts): naver

---
+++ test/specs/components/dictionaries/naver/requests.mock.ts
@@ -0,0 +1,25 @@
+import { MockRequest } from '@/components/dictionaries/helpers'
+
+export const mockSearchTexts = ['爱', '愛']
+
+export const mockRequest: MockRequest = mock => {
+  mock
+    .onGet(new RegExp(encodeURIComponent('爱')))
+    .reply(
+      200,
+      new DOMParser().parseFromString(
+        require(`raw-loader!./response/爱.html`).default,
+        'text/html'
+      )
+    )
+
+  mock
+    .onGet(new RegExp(encodeURIComponent('愛')))
+    .reply(
+      200,
+      new DOMParser().parseFromString(
+        require(`raw-loader!./response/愛.html`).default,
+        'text/html'
+      )
+    )
+}
