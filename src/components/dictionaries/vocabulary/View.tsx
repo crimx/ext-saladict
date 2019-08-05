84d23432f5bb460c4856f9e60ae4721cdf7384de
crimx <straybugs@gmail.com>
2019-08-05 22:06:24 +0800

    refactor(dicts): vocabulary

--- src/components/dictionaries/vocabulary/View.tsx
+++ src/components/dictionaries/vocabulary/View.tsx
@@ -1,15 +1,12 @@
-import React from 'react'
+import React, { FC } from 'react'
 import { VocabularyResult } from './engine'
 import { ViewPorps } from '@/components/dictionaries/helpers'
 
-export default class DictVocabulary extends React.PureComponent<ViewPorps<VocabularyResult>> {
-  render () {
-    const { long, short } = this.props.result
-    return (
-      <>
-        <p className='dictVocabulary-Short'>{short}</p>
-        <p className='dictVocabulary-Long'>{long}</p>
-      </>
-    )
-  }
-}
+export const DictVocabulary: FC<ViewPorps<VocabularyResult>> = ({ result }) => (
+  <>
+    <p className="dictVocabulary-Short">{result.short}</p>
+    <p className="dictVocabulary-Long">{result.long}</p>
+  </>
+)
+
+export default DictVocabulary
