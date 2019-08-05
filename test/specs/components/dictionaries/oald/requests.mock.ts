3d2d79a3411417923afa76ada5612eae58fc1745
crimx <straybugs@gmail.com>
2019-08-05 12:07:48 +0800

    refactor(dicts): oald

---
+++ test/specs/components/dictionaries/oald/requests.mock.ts
@@ -0,0 +1,20 @@
+import { MockRequest } from '@/components/dictionaries/helpers'
+
+export const mockSearchTexts = ['love', 'jumblish']
+
+export const mockRequest: MockRequest = mock => {
+  mock.onGet(/oxfordlearnersdictionaries/).reply(info => {
+    const wordMatch = /[^\/]+$/.exec(info.url || '')
+    return wordMatch
+      ? [
+          200,
+          new DOMParser().parseFromString(
+            require(`raw-loader!./response/${decodeURIComponent(
+              wordMatch[0]
+            )}.html`).default,
+            'text/html'
+          )
+        ]
+      : [404]
+  })
+}
