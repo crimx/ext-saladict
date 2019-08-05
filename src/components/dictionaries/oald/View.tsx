d0fe83a1f6f693d3647d58d9450dcd9e23e7a5a4
crimx <straybugs@gmail.com>
2019-08-05 12:07:48 +0800

    refactor(dicts): oald

--- src/components/dictionaries/oald/View.tsx
+++ src/components/dictionaries/oald/View.tsx
@@ -1,56 +1,74 @@
-import React from 'react'
+import React, { FC } from 'react'
 import Speaker from '@/components/Speaker'
 import { OALDResult, OALDResultLex, OALDResultRelated } from './engine'
 import { ViewPorps } from '@/components/dictionaries/helpers'
 
-export default class DictOALD extends React.PureComponent<ViewPorps<OALDResult>> {
-  handleEntryClick = (e: React.MouseEvent<HTMLElement>) => {
-    const target = e.nativeEvent.target as HTMLSpanElement
-    if (target.classList && target.classList.contains('heading')) {
-      (target.parentElement as HTMLDivElement).classList.toggle('is-active')
-      this.props.recalcBodyHeight()
-    }
-  }
-
-  renderLex (result: OALDResultLex) {
-    return result.items.map(entry => (
-      <section key={entry.title} className='dictOALD-Entry' onClick={this.handleEntryClick}>
-        <header className='dictOALD-Header'>
-          <div className='webtop-g' dangerouslySetInnerHTML={{ __html: entry.title }} />
-          {entry.prons.length > 0 &&
-            <div className='pron-gs ei-g'>
-              {entry.prons.map(p => (
-                <React.Fragment key={p.phsym}>
-                  <span className='pron-g' dangerouslySetInnerHTML={{ __html: p.phsym }} />
-                  <Speaker src={p.pron} />
-                </React.Fragment>
-              ))}
-            </div>
-          }
-        </header>
-        <span className='h-g' dangerouslySetInnerHTML={{ __html: entry.defs }} />
-      </section>
-    ))
-  }
-
-  renderRelated (result: OALDResultRelated) {
-    return (
-      <>
-        <p>Did you mean:</p>
-        <ul className='dictOALD-Related' dangerouslySetInnerHTML={{ __html: result.list }} />
-      </>
-    )
-  }
-
-  render () {
-    const { result } = this.props
-    switch (result.type) {
-      case 'lex':
-        return this.renderLex(result)
-      case 'related':
-        return this.renderRelated(result)
-      default:
-        return null
-    }
+export const DictOALD: FC<ViewPorps<OALDResult>> = ({ result }) => {
+  switch (result.type) {
+    case 'lex':
+      return renderLex(result)
+    case 'related':
+      return renderRelated(result)
+    default:
+      return null
   }
 }
+
+function renderLex(result: OALDResultLex) {
+  return (
+    <>
+      {result.items.map(entry => (
+        <section
+          key={entry.title}
+          className="dictOALD-Entry"
+          onClick={e => {
+            const target = e.nativeEvent.target as HTMLSpanElement
+            if (target.classList && target.classList.contains('heading')) {
+              ;(target.parentElement as HTMLDivElement).classList.toggle(
+                'is-active'
+              )
+            }
+          }}
+        >
+          <header className="dictOALD-Header">
+            <div
+              className="webtop-g"
+              dangerouslySetInnerHTML={{ __html: entry.title }}
+            />
+            {entry.prons.length > 0 && (
+              <div className="pron-gs ei-g">
+                {entry.prons.map(p => (
+                  <React.Fragment key={p.phsym}>
+                    <span
+                      className="pron-g"
+                      dangerouslySetInnerHTML={{ __html: p.phsym }}
+                    />
+                    <Speaker src={p.pron} />
+                  </React.Fragment>
+                ))}
+              </div>
+            )}
+          </header>
+          <span
+            className="h-g"
+            dangerouslySetInnerHTML={{ __html: entry.defs }}
+          />
+        </section>
+      ))}
+    </>
+  )
+}
+
+function renderRelated(result: OALDResultRelated) {
+  return (
+    <>
+      <p>Did you mean:</p>
+      <ul
+        className="dictOALD-Related"
+        dangerouslySetInnerHTML={{ __html: result.list }}
+      />
+    </>
+  )
+}
+
+export default DictOALD
--- src/components/dictionaries/oald/engine.ts
+++ src/components/dictionaries/oald/engine.ts
@@ -2,20 +2,23 @@ import { fetchDirtyDOM } from '@/_helpers/fetch-dom'
 import { reflect } from '@/_helpers/promise-more'
 import {
   HTMLString,
-  getInnerHTMLBuilder,
+  getInnerHTML,
   handleNoResult,
   handleNetWorkError,
   SearchFunction,
   GetSrcPageFunction,
+  DictSearchResult
 } from '../helpers'
 import { DictConfigs } from '@/app-config'
-import { DictSearchResult } from '@/typings/server'
 
-export const getSrcPage: GetSrcPageFunction = (text) => {
-  return `https://www.oxfordlearnersdictionaries.com/definition/english/${text.trim().split(/\s+/).join('-')}`
+export const getSrcPage: GetSrcPageFunction = text => {
+  return `https://www.oxfordlearnersdictionaries.com/definition/english/${text
+    .trim()
+    .split(/\s+/)
+    .join('-')}`
 }
 
-const getInnerHTML = getInnerHTMLBuilder('https://www.oxfordlearnersdictionaries.com/')
+const HOST = 'https://www.oxfordlearnersdictionaries.com'
 
 interface OALDResultItem {
   title: HTMLString
@@ -41,32 +44,36 @@ export type OALDResult = OALDResultLex | OALDResultRelated
 type OALDSearchResult = DictSearchResult<OALDResult>
 type OALDSearchResultLex = DictSearchResult<OALDResultLex>
 
-export const search: SearchFunction<OALDSearchResult> = (
-  text, config, profile, payload
+export const search: SearchFunction<OALDResult> = (
+  text,
+  config,
+  profile,
+  payload
 ) => {
   text = text.toLocaleLowerCase().replace(/[^A-Za-z0-9]+/g, '-')
   const options = profile.dicts.all.oald.options
 
-  return fetchDirtyDOM('https://www.oxfordlearnersdictionaries.com/definition/english/' + text)
+  return fetchDirtyDOM(
+    'https://www.oxfordlearnersdictionaries.com/definition/english/' + text
+  )
     .catch(handleNetWorkError)
     .then(doc => checkResult(doc, text, options))
 }
 
-function checkResult (
+function checkResult(
   doc: Document,
   text: string,
-  options: DictConfigs['oald']['options'],
+  options: DictConfigs['oald']['options']
 ): OALDSearchResult | Promise<OALDSearchResult> {
   if (doc.querySelector('#entryContent')) {
-    return getAllEntries(doc, text)
-      .then(handleAllDOMs)
+    return getAllEntries(doc, text).then(handleAllDOMs)
   } else if (options.related) {
     const $alternative = doc.querySelector('.result-list')
     if ($alternative) {
       return {
         result: {
           type: 'related',
-          list: getInnerHTML($alternative)
+          list: getInnerHTML(HOST, $alternative)
         }
       }
     }
@@ -74,32 +81,38 @@ function checkResult (
   return handleNoResult()
 }
 
-function getAllEntries (
-  doc: Document,
-  text: string,
-): Promise<Document[]> {
+function getAllEntries(doc: Document, text: string): Promise<Document[]> {
   const wordTester = new RegExp(`/${text}_\\d+$`)
-  const hrefs = [...doc.querySelectorAll<HTMLAnchorElement>('#relatedentries .list-col a')]
+  const hrefs = [
+    ...doc.querySelectorAll<HTMLAnchorElement>('#relatedentries .list-col a')
+  ]
     .map($a => $a.getAttribute('href') || '')
     .filter(href => wordTester.test(href))
 
   if (hrefs.length > 0) {
-    return reflect(hrefs.map(href => fetchDirtyDOM(href)))
-      .then(docs => [doc, ...docs.filter((d): d is Document => d as any as boolean)])
+    return reflect(hrefs.map(href => fetchDirtyDOM(href))).then(docs => [
+      doc,
+      ...docs.filter((d): d is Document => !!d)
+    ])
   }
 
   return Promise.resolve([doc])
 }
 
-function handleAllDOMs (
-  docs: Document[],
+function handleAllDOMs(
+  docs: Document[]
 ): OALDSearchResultLex | Promise<OALDSearchResultLex> {
-  const results = docs.map(handleDOM)
-    .filter((result): result is DictSearchResult<OALDResultItem> => result as any as boolean)
+  const results = docs
+    .map(handleDOM)
+    .filter((result): result is DictSearchResult<OALDResultItem> => !!result)
   if (results.length > 0) {
-    let resultWithAudio = results.find(({ audio }) => (audio && audio.uk && audio.us) as any as boolean)
+    let resultWithAudio = results.find(
+      ({ audio }) => audio && audio.uk && audio.us
+    )
     if (!resultWithAudio) {
-      resultWithAudio = results.find(({ audio }) => (audio && (audio.uk || audio.us)) as any as boolean)
+      resultWithAudio = results.find(
+        ({ audio }) => audio && (audio.uk || audio.us)
+      )
     }
     const audio = resultWithAudio ? resultWithAudio.audio : undefined
     return {
@@ -107,35 +120,41 @@ function handleAllDOMs (
         type: 'lex',
         items: results.map(({ result }) => result)
       },
-      audio,
+      audio
     }
   }
   return handleNoResult()
 }
 
-function handleDOM (
-  doc: Document,
-): null | DictSearchResult<OALDResultItem> {
+function handleDOM(doc: Document): null | DictSearchResult<OALDResultItem> {
   const result: OALDResultItem = {
-    title: getInnerHTML(doc, '.webtop-g'),
+    title: getInnerHTML(HOST, doc, '.webtop-g'),
     prons: [],
-    defs: '',
+    defs: ''
   }
 
-  const audio: { us?: string, uk?: string } = {}
+  const audio: { us?: string; uk?: string } = {}
 
   const $entry = doc.querySelector('#entryContent .h-g')
-  if (!$entry) { return null }
+  if (!$entry) {
+    return null
+  }
   const $topCon = $entry.querySelector('.top-container')
-  if (!$topCon) { return null }
+  if (!$topCon) {
+    return null
+  }
 
   const $prons = $topCon.querySelector('.pron-gs')
   if ($prons) {
     $prons.querySelectorAll('.pron-g').forEach($p => {
       const $audio = $p.querySelector<HTMLDivElement>('.audio_play_button')
-      if (!$audio) { return }
+      if (!$audio) {
+        return
+      }
       const mp3 = $audio.dataset.srcMp3
-      if (!mp3) { return }
+      if (!mp3) {
+        return
+      }
 
       if ($audio.className.includes('pron-uk')) {
         audio.uk = mp3
@@ -143,12 +162,12 @@ function handleDOM (
         audio.us = mp3
       }
 
-      result.prons.push({ phsym: getInnerHTML($p), pron: mp3 })
+      result.prons.push({ phsym: getInnerHTML(HOST, $p), pron: mp3 })
     })
   }
 
   $topCon.remove()
-  result.defs = getInnerHTML($entry)
+  result.defs = getInnerHTML(HOST, $entry)
 
   if (result.title && result.defs) {
     return { result, audio }
