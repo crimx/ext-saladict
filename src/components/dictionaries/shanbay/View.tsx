b45cf4255b252dfe2382f8cbde1855facffb3a24
crimx <straybugs@gmail.com>
2019-08-05 12:39:19 +0800

    refactor(dicts): shanbay

--- src/components/dictionaries/shanbay/View.tsx
+++ src/components/dictionaries/shanbay/View.tsx
@@ -1,61 +1,49 @@
-import React from 'react'
+import React, { FC } from 'react'
 import Speaker from '@/components/Speaker'
-import { ShanbayResult, ShanbayResultLex } from './engine'
+import { ShanbayResult } from './engine'
 import { ViewPorps } from '@/components/dictionaries/helpers'
 
-export default class DictShanbay extends React.PureComponent<ViewPorps<ShanbayResult>> {
-  renderLex (result: ShanbayResultLex) {
-    return (
-      <>
-        {result.title &&
-          <div className='dictShanbay-HeaderContainer'>
-            <h1 className='dictShanbay-Title'>{result.title}</h1>
-            <span className='dictShanbay-Pattern'>{result.pattern}</span>
-          </div>
-        }
-        {result.prons.length > 0 &&
-          <div className='dictShanBay-HeaderContainer'>
-            {result.prons.map(({ phsym, url }) => (
-              <React.Fragment key={url}>
-                {phsym} <Speaker src={url} />
-              </React.Fragment>
-            ))}
-          </div>
-        }
-        {result.basic &&
-          <div className='dictShanbay-Basic' dangerouslySetInnerHTML={{ __html: result.basic }} />
-        }
-        {result.sentences &&
-          <div>
-            <h1 className='dictShanbay-SecTitle'>权威例句</h1>
-            <ol className='dictShanbay-Sentence'>
-            { this.renderSentences(result.sentences) }
-            </ol>
-          </div>
-        }
-      </>
-    )
-  }
+export const DictShanbay: FC<ViewPorps<ShanbayResult>> = ({ result }) => (
+  <>
+    {result.title && (
+      <div className="dictShanbay-HeaderContainer">
+        <h1 className="dictShanbay-Title">{result.title}</h1>
+        <span className="dictShanbay-Pattern">{result.pattern}</span>
+      </div>
+    )}
+    {result.prons.length > 0 && (
+      <div className="dictShanBay-HeaderContainer">
+        {result.prons.map(({ phsym, url }) => (
+          <React.Fragment key={url}>
+            {phsym} <Speaker src={url} />
+          </React.Fragment>
+        ))}
+      </div>
+    )}
+    {result.basic && (
+      <div
+        className="dictShanbay-Basic"
+        dangerouslySetInnerHTML={{ __html: result.basic }}
+      />
+    )}
+    {result.sentences && (
+      <div>
+        <h1 className="dictShanbay-SecTitle">权威例句</h1>
+        <ol className="dictShanbay-Sentence">
+          {result.sentences.map(sentence => (
+            <li key={sentence.annotation}>
+              <p
+                dangerouslySetInnerHTML={{
+                  __html: sentence.annotation
+                }}
+              />
+              <p>{sentence.translation}</p>
+            </li>
+          ))}
+        </ol>
+      </div>
+    )}
+  </>
+)
 
-  renderSentences (sentences) {
-    return sentences.map((sentence: {
-      annotation: string,
-      translation: string,
-    }) => {
-      return (<li>
-        <p dangerouslySetInnerHTML={{ __html: sentence.annotation }} />
-        <p>{ sentence.translation }</p>
-      </li>)
-    })
-  }
-
-  render () {
-    const { result } = this.props
-    switch (result.type) {
-      case 'lex':
-        return this.renderLex(result)
-      default:
-        return null
-    }
-  }
-}
+export default DictShanbay
