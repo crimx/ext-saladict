75c1ee2ba2df67db35387ca2a6ca9b66b5f5b7e2
crimx <straybugs@gmail.com>
2019-08-05 10:24:46 +0800

    refactor(dicts): macmillan

--- src/components/dictionaries/macmillan/View.tsx
+++ src/components/dictionaries/macmillan/View.tsx
@@ -1,43 +1,56 @@
-import React from 'react'
+import React, { FC } from 'react'
 import Speaker from '@/components/Speaker'
 import StarRates from '@/components/StarRates'
-import { MacmillanResult, MacmillanResultLex, MacmillanResultRelated } from './engine'
+import {
+  MacmillanResult,
+  MacmillanResultLex,
+  MacmillanResultRelated
+} from './engine'
 import { ViewPorps } from '@/components/dictionaries/helpers'
 
-export default class DictMacmillan extends React.PureComponent<ViewPorps<MacmillanResult>> {
-  renderLex (result: MacmillanResultLex) {
-    return result.items.map(def => (
-      <section key={def.senses}>
-        <h1 className='dictMacmillan-Title'>{def.title}</h1>
-        <header className='dictMacmillan-Header'>
-          {def.ratting && <StarRates rate={def.ratting} />}
-          <span className='dictMacmillan-Header_Info'>
-            {def.phsym} <Speaker src={def.pron} /> {def.pos} {def.sc}
-          </span>
-        </header>
-        <ol className='dictMacmillan-Sences' dangerouslySetInnerHTML={{ __html: def.senses }} />
-      </section>
-    ))
+export const DictMacmillan: FC<ViewPorps<MacmillanResult>> = ({ result }) => {
+  switch (result.type) {
+    case 'lex':
+      return renderLex(result)
+    case 'related':
+      return renderRelated(result)
+    default:
+      return null
   }
+}
 
-  renderRelated (result: MacmillanResultRelated) {
-    return (
-      <>
-        <p>Did you mean:</p>
-        <ul className='dictMacmillan-Related' dangerouslySetInnerHTML={{ __html: result.list }} />
-      </>
-    )
-  }
+export default DictMacmillan
 
-  render () {
-    const { result } = this.props
-    switch (result.type) {
-      case 'lex':
-        return this.renderLex(result)
-      case 'related':
-        return this.renderRelated(result)
-      default:
-        return null
-    }
-  }
+function renderLex(result: MacmillanResultLex) {
+  return (
+    <>
+      {result.items.map(def => (
+        <section key={def.senses}>
+          <h1 className="dictMacmillan-Title">{def.title}</h1>
+          <header className="dictMacmillan-Header">
+            {def.ratting && <StarRates rate={def.ratting} />}
+            <span className="dictMacmillan-Header_Info">
+              {def.phsym} <Speaker src={def.pron} /> {def.pos} {def.sc}
+            </span>
+          </header>
+          <ol
+            className="dictMacmillan-Sences"
+            dangerouslySetInnerHTML={{ __html: def.senses }}
+          />
+        </section>
+      ))}
+    </>
+  )
+}
+
+function renderRelated(result: MacmillanResultRelated) {
+  return (
+    <>
+      <p>Did you mean:</p>
+      <ul
+        className="dictMacmillan-Related"
+        dangerouslySetInnerHTML={{ __html: result.list }}
+      />
+    </>
+  )
 }
