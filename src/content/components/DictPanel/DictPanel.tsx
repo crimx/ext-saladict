37b89c728abf517897b8093d8c0d70ba20b9c27d
crimx <straybugs@gmail.com>
2019-08-17 18:40:12 +0800

    refactor(panel): dict panel integration

---
+++ src/content/components/DictPanel/DictPanel.tsx
@@ -0,0 +1,55 @@
+import React, { FC, ReactNode, useState } from 'react'
+import ResizeReporter from 'react-resize-reporter'
+
+export interface DictPanelProps {
+  x: number
+  y: number
+  width: number
+  maxHeight: number
+
+  withAnimation: boolean
+
+  menuBar: ReactNode
+  mtaBox: ReactNode
+  dictList: ReactNode
+  waveformBox: ReactNode
+
+  onHeightChanged: (height: number) => void
+}
+
+export const DictPanel: FC<DictPanelProps> = props => {
+  const [height, setHeight] = useState(40)
+
+  return (
+    <div
+      className={`dictPanel-Root${props.withAnimation ? ' isAnimate' : ''}`}
+      style={{
+        left: props.x,
+        top: props.y,
+        width: props.width,
+        maxHeight: props.maxHeight,
+        backgroundColor: '#fff',
+        color: '#333',
+        '--panel-background-color': '#fff',
+        '--panel-color': '#333',
+        '--panel-width': props.width + 'px',
+        '--panel-height': height + 'px'
+      }}
+    >
+      <ResizeReporter
+        onHeightChanged={height => {
+          setHeight(height)
+          props.onHeightChanged(height)
+        }}
+        reportInit
+        debounce={10}
+      />
+      {props.menuBar}
+      <div className="dictPanel-Body">
+        {props.mtaBox}
+        {props.dictList}
+      </div>
+      {props.waveformBox}
+    </div>
+  )
+}
