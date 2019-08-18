0baa27c1ffc1e63e54a32c17a7bd913574529997
crimx <straybugs@gmail.com>
2019-08-18 10:23:59 +0800

    refactor(content): rename action handlers

--- src/content/redux/utils/createReducer.ts
+++ src/content/redux/utils/createReducer.ts
@@ -1,9 +1,9 @@
 import { StoreActionCatalog, StoreAction } from '../modules'
-import { ActionHandler } from '../utils/types'
+import { ActionHandlers } from '../utils/types'
 
 export const createReducer = <C extends {}, S extends {}>(
   initialState: S,
-  handlers: ActionHandler<C, S, StoreActionCatalog>
+  handlers: ActionHandlers<C, S, StoreActionCatalog>
 ) =>
   function reducer(state = initialState, action: StoreAction): S {
     if (handlers.hasOwnProperty(action.type)) {
--- src/content/redux/utils/types.ts
+++ src/content/redux/utils/types.ts
@@ -12,6 +12,7 @@ export type ActionType<P extends ActionCatalog> = keyof P
 
 export type Action<
   C extends ActionCatalog,
+  /** Action type. If ignored a union of all actions will be returned */
   T extends keyof C = keyof C
 > = T extends any // 'extends' hack to generate union
   ? Readonly<
@@ -28,7 +29,16 @@ export type Action<
 export type ActionHandler<
   /** module actions */
   C extends ActionCatalog,
-  /** state */
+  /** module state */
+  S extends {},
+  /** action type */
+  T extends keyof C
+> = (state: Readonly<S>, action: Action<C, T>) => Readonly<S>
+
+export type ActionHandlers<
+  /** module actions */
+  C extends ActionCatalog,
+  /** module state */
   S extends {},
   /** all actions */
   SC extends {} = {}
@@ -42,6 +52,7 @@ export type ActionHandler<
     ) => Readonly<S>
   }
 
+/** Perform init operations e.g. setup listeners */
 export type Init<C extends ActionCatalog> = (
   dispatch: Dispatch<Action<C>>
 ) => void
