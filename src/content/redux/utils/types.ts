7c5fb5e6be6381b822785cfe6b21742a250c2432
crimx <straybugs@gmail.com>
2019-08-18 09:53:47 +0800

    fix: correctly made payload and meta optional

--- src/content/redux/utils/types.ts
+++ src/content/redux/utils/types.ts
@@ -3,7 +3,7 @@ import { Dispatch } from 'redux'
 /** Default action catalog */
 export interface ActionCatalog {
   [type: string]: {
-    payload: any
+    payload?: any
     meta?: any
   }
 }
@@ -14,16 +14,15 @@ export type Action<
   C extends ActionCatalog,
   T extends keyof C = keyof C
 > = T extends any // 'extends' hack to generate union
-  ? Readonly<{
-      type: T
-      payload: 'payload' extends keyof C[T]
-        ? C[T][Extract<'payload', keyof C[T]>]
-        : undefined
-      error?: boolean
-      meta?: 'meta' extends keyof C[T]
-        ? C[T][Extract<'meta', keyof C[T]>]
-        : undefined
-    }>
+  ? Readonly<
+      // prettier-ignore
+      {
+        type: T
+        error?: boolean
+      } &
+      ('payload' extends keyof C[T] ? Pick<C[T], 'payload'> : { payload?: undefined }) &
+      ('meta' extends keyof C[T] ? Pick<C[T], 'meta'> : { meta?: undefined })
+    >
   : never
 
 export type ActionHandler<
