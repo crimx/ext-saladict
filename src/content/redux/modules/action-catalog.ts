82a4c4d3c93fe0e6c99cf99874f80042e754c077
crimx <straybugs@gmail.com>
2019-08-20 19:30:37 +0800

    refactor(content): redux init

--- src/content/redux/modules/action-catalog.ts
+++ src/content/redux/modules/action-catalog.ts
@@ -7,20 +7,34 @@ export type ActionCatalog = {
   NEW_CONFIG: {
     payload: AppConfig
   }
+
   NEW_PROFILE: {
     payload: Profile
   }
+
   NEW_SELECTION: {
     payload: Message<'SELECTION'>['payload']
   }
+
+  /** Is App temporary disabled */
+  TEMP_DISABLED_STATE: {
+    payload: boolean
+  }
+
   /** Click or hover on salad bowl */
   BOWL_ACTIVATED: {}
-  SEARCH_END: {
-    payload: {
-      id: DictID
-      result: any
-    }
+
+  /* ------------------------------------------------ *\
+     Dict Panel
+  \* ------------------------------------------------ */
+
+  CLOSE_PANEL: {}
+
+  /** Is current word in Notebook */
+  WORD_IN_NOTEBOOK: {
+    payload: boolean
   }
+
   SEARCH_START: {
     payload?: {
       /** Search with specific dict */
@@ -31,8 +45,26 @@ export type ActionCatalog = {
       payload?: any
     }
   }
-  /** Is current word in Notebook */
-  WORD_IN_NOTEBOOK: {
+
+  SEARCH_END: {
+    payload: {
+      id: DictID
+      result: any
+    }
+  }
+
+  /* ------------------------------------------------ *\
+    Quick Search Dict Panel
+  \* ------------------------------------------------ */
+
+  SUMMONED_PANEL_INIT: {
+    /** search text */
+    payload: string
+  }
+
+  QS_PANEL_CHANGED: {
     payload: boolean
   }
+
+  OPEN_QS_PANEL: {}
 }
