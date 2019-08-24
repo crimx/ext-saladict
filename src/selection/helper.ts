f6dd8e45283fa9583072d0b7733ee4afe807ab0e
crimx <straybugs@gmail.com>
2019-08-24 19:48:04 +0800

    refactor(selection): instant capture and mouse events

--- src/selection/helper.ts
+++ src/selection/helper.ts
@@ -1,120 +1,51 @@
 import { AppConfig } from '@/app-config'
-import { createConfigStream } from '@/_helpers/config-manager'
-import { message } from '@/_helpers/browser-api'
-import { getDefaultSelectionInfo, SelectionInfo } from '@/_helpers/selection'
-import { MsgType, PostMsgType, PostMsgSelection, MsgSelection } from '@/typings/message'
-
-import { Observable } from 'rxjs/Observable'
-import { fromEvent } from 'rxjs/observable/fromEvent'
-import { merge } from 'rxjs/observable/merge'
-import { of } from 'rxjs/observable/of'
-import { map } from 'rxjs/operators/map'
-import { mapTo } from 'rxjs/operators/mapTo'
-import { share } from 'rxjs/operators/share'
-import { filter } from 'rxjs/operators/filter'
-import { distinctUntilChanged } from 'rxjs/operators/distinctUntilChanged'
+import { Observable, fromEvent, merge, of } from 'rxjs'
+import { map, mapTo, filter, distinctUntilChanged } from 'rxjs/operators'
 
 const isMac = /mac/i.test(navigator.platform)
-
-export const config$$ = share<AppConfig>()(createConfigStream())
-
-export function sendMessage (
-  msg: {
-    selectionInfo: SelectionInfo
-    mouseX: number
-    mouseY: number
-    self: boolean
-    dbClick?: boolean
-    shiftKey?: boolean
-    ctrlKey?: boolean
-    metaKey?: boolean
-    instant?: boolean
-  }
-) {
-  if (window.parent === window) {
-    // top
-    if (process.env.NODE_ENV === 'development') {
-      console.log('New selection', msg)
-    }
-
-    message.self.send<MsgSelection>({
-      dbClick: false,
-      shiftKey: false,
-      ctrlKey: false,
-      metaKey: false,
-      instant: false,
-      ...msg,
-      force: false,
-      type: MsgType.Selection,
-    })
-  } else {
-    // post to upper frames/window
-    window.parent.postMessage({
-      ...msg,
-      type: PostMsgType.Selection,
-    } as PostMsgSelection, '*')
-  }
-}
-
-export function sendEmptyMessage (isDictPanel: boolean) {
-  // empty message
-  const msg: MsgSelection = {
-    type: MsgType.Selection,
-    selectionInfo: getDefaultSelectionInfo(),
-    self: isDictPanel,
-    mouseX: 0,
-    mouseY: 0,
-    dbClick: false,
-    shiftKey: false,
-    ctrlKey: false,
-    metaKey: false,
-    instant: false,
-    force: false,
-  }
-
-  if (process.env.NODE_ENV === 'development') {
-    console.log('New selection', msg)
-  }
-
-  message.self.send(msg)
-}
 
 /**
  * Is quick search key pressed(command on mac, ctrl on others)
  */
-export function isQSKey (evt: KeyboardEvent): boolean {
+export function isQSKey(evt: KeyboardEvent): boolean {
   return isMac ? evt.key === 'Meta' : evt.key === 'Control'
 }
 
 /**
  * Is esc button pressed
  */
-export function isEscapeKey (evt: KeyboardEvent): boolean {
+export function isEscapeKey(evt: KeyboardEvent): boolean {
   return evt.key === 'Escape'
 }
 
-export function isKeyPressed (keySelectior: (e: KeyboardEvent) => boolean): Observable<true> {
+export function isKeyPressed(
+  keySelectior: (e: KeyboardEvent) => boolean
+): Observable<true> {
   return merge(
-    map(keySelectior)(fromEvent<KeyboardEvent>(window, 'keydown', { capture: true })),
+    map(keySelectior)(
+      fromEvent<KeyboardEvent>(window, 'keydown', { capture: true })
+    ),
     mapTo(false)(fromEvent(window, 'keyup', { capture: true })),
     mapTo(false)(fromEvent(window, 'blur', { capture: true })),
-    of(false),
+    of(false)
   ).pipe(
     distinctUntilChanged(), // ignore long press
-    filter((x): x is true => x),
+    filter((x): x is true => x)
   )
 }
 
-export function isTypeField (event: MouseEvent | TouchEvent | null): boolean {
+// common editors
+const editorTester = /CodeMirror|ace_editor|monaco-editor/
+
+export function isTypeField(event: MouseEvent | TouchEvent | null): boolean {
   if (event && event.target) {
-    const target = event.target
-    if (target['tagName'] === 'INPUT' || target['tagName'] === 'TEXTAREA') {
+    const target = event.target as Element
+    if (target.tagName === 'INPUT' || target.tagName === 'TEXTAREA') {
       return true
     }
 
-    const editorTester = /CodeMirror|ace_editor|monaco-editor/
     // Popular code editors CodeMirror, ACE and Monaco
-    for (let el = target as Element | null; el; el = el.parentElement) {
+    for (let el: Element | null = target; el; el = el.parentElement) {
       // With CodeMirror the `pre.CodeMirror-line` somehow got detached when the event
       // triggerd. So el will never reach the root `.CodeMirror`.
       if (editorTester.test(el.className)) {
@@ -126,28 +57,11 @@ export function isTypeField (event: MouseEvent | TouchEvent | null): boolean {
   return false
 }
 
-/**
- * Is inside dict panel on a Saladict internal page
- */
-export function isInPanelOnInternalPage (
-  event: MouseEvent | TouchEvent | null
-): boolean {
-  if (event && event.target) {
-    const target = event.target
-    if (target['classList']) {
-      for (let el = target as Element | null; el; el = el.parentElement) {
-        if (el.classList.contains('saladict-DictPanel')) {
-          return true
-        }
-      }
-    }
-  }
-  return false
-}
-
-export function isBlacklisted (config: AppConfig): boolean {
+export function isBlacklisted(config: AppConfig): boolean {
   const url = window.pageURL || document.URL || ''
-  if (!url) { return false }
+  if (!url) {
+    return false
+  }
   return (
     config.blacklist.some(([r]) => new RegExp(r).test(url)) &&
     config.whitelist.every(([r]) => !new RegExp(r).test(url))
---
+++ src/selection/message.ts
@@ -0,0 +1,88 @@
+import { newWord } from '@/_helpers/record-manager'
+import { Message } from '@/typings/message'
+import { message } from '@/_helpers/browser-api'
+
+interface PostMessageEvent extends MessageEvent {
+  data: {
+    type: 'SALADICT_SELECTION'
+    payload: Message<'SELECTION'>['payload']
+  }
+}
+
+export function messageHandler({ data, source }: PostMessageEvent) {
+  if (!data || data.type !== 'SALADICT_SELECTION') {
+    return
+  }
+
+  // get the souce iframe
+  const matchSrc = ({ contentWindow }: HTMLIFrameElement | HTMLFrameElement) =>
+    contentWindow === source
+
+  const frame =
+    Array.from(document.querySelectorAll('iframe')).find(matchSrc) ||
+    Array.from(document.querySelectorAll('frame')).find(matchSrc)
+
+  if (!frame) {
+    return
+  }
+
+  const { left, top } = frame.getBoundingClientRect()
+  data.payload.mouseX = data.payload.mouseX + left
+  data.payload.mouseY = data.payload.mouseY + top
+  sendMessage(data.payload)
+}
+
+/**
+ * Send to upper frame for calculating offset.
+ * Finally send to dict panel.
+ */
+export function sendMessage(payload: Message<'SELECTION'>['payload']) {
+  if (window.parent === window) {
+    // top
+    if (process.env.NODE_ENV === 'development') {
+      console.log('New selection', payload)
+    }
+
+    message.self.send({
+      type: 'SELECTION',
+      payload
+    })
+  } else {
+    // post to upper frames/window
+    window.parent.postMessage(
+      {
+        type: 'SALADICT_SELECTION',
+        payload
+      },
+      '*'
+    )
+  }
+}
+
+/**
+ * Send a
+ */
+export function sendEmptyMessage(isDictPanel: boolean) {
+  // empty message
+  const msg: Message<'SELECTION'> = {
+    type: 'SELECTION',
+    payload: {
+      word: newWord(),
+      self: isDictPanel,
+      mouseX: 0,
+      mouseY: 0,
+      dbClick: false,
+      shiftKey: false,
+      ctrlKey: false,
+      metaKey: false,
+      instant: false,
+      force: false
+    }
+  }
+
+  if (process.env.NODE_ENV === 'development') {
+    console.log('New selection', msg.payload)
+  }
+
+  return message.self.send(msg)
+}
