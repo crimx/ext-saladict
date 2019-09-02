2caa69ff226338e423a3d80d831c63fc541b3b64
crimx <straybugs@gmail.com>
2019-09-02 12:01:35 +0800

    refactor(content): update types

--- src/content/redux/utils/operators.ts
+++ src/content/redux/utils/operators.ts
@@ -24,7 +24,14 @@ export function ofType<T extends StoreActionType>(
 ) => Observable<Action<StoreActionCatalog, T>>
 export function ofType<T extends StoreActionType>(
   ...keys: Array<T | { toString: () => T }>
+): (
+  source: Observable<StoreAction>
+) => Observable<Action<StoreActionCatalog, T>>
+export function ofType<T extends StoreActionType>(
+  ...keys: Array<T | { toString: () => T }>
 ) {
   return (source: Observable<StoreAction>) =>
+    // function type coercion
+    // eslint-disable-next-line eqeqeq
     filter<StoreAction>(({ type }) => keys.some(key => type == key))(source)
 }
