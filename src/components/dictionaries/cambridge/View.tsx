4174df5895c2ead9da2a997f3f5f279dec73e146
crimx <straybugs@gmail.com>
2019-09-13 01:15:28 +0800

    refactor(dicts): update cambridge api

--- src/components/dictionaries/cambridge/View.tsx
+++ src/components/dictionaries/cambridge/View.tsx
@@ -1,31 +1,16 @@
 import React, { FC } from 'react'
-import Speaker from '@/components/Speaker'
 import { CambridgeResult } from './engine'
 import { ViewPorps } from '@/components/dictionaries/helpers'
 
 export const DictCambridge: FC<ViewPorps<CambridgeResult>> = props => (
   <>
-    {props.result.map(entry => (
+    {props.result.map((entry, i) => (
       <section
-        key={entry.defs}
+        key={i}
         className="dictCambridge-Entry"
         onClick={handleEntryClick}
       >
-        <header className="dictCambridge-Header">
-          <h1 className="dictCambridge-Title">{entry.title}</h1>
-          <span dangerouslySetInnerHTML={{ __html: entry.pos }} />
-        </header>
-        {entry.prons.length > 0 && (
-          <div className="dictCambridge-Prons">
-            {entry.prons.map((p, i) => (
-              <React.Fragment key={p.pron}>
-                {p.phsym} <Speaker src={p.pron} />{' '}
-                {p.phsym.startsWith('us') ? <br /> : null}
-              </React.Fragment>
-            ))}
-          </div>
-        )}
-        <div dangerouslySetInnerHTML={{ __html: entry.defs }} />
+        <div dangerouslySetInnerHTML={{ __html: entry }} />
       </section>
     ))}
   </>
