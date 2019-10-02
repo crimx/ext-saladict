c86e6e8e4210e96d03fb74dc58a68f720781b36d
crimx <straybugs@gmail.com>
2019-10-02 18:30:56 +0800

    feat(panel): add touch mode close #492

--- src/selection/select-text.ts
+++ src/selection/select-text.ts
@@ -8,7 +8,9 @@ import {
   switchMap,
   scan,
   startWith,
-  throttle
+  throttle,
+  delay,
+  distinctUntilChanged
 } from 'rxjs/operators'
 import { AppConfig } from '@/app-config'
 import { isInDictPanel, isInSaladictExternal } from '@/_helpers/saladict'
@@ -49,6 +51,58 @@ export function createSelectTextStream(config: AppConfig | null) {
     ),
     scan((sum: number, flag: boolean) => (flag ? sum + 1 : 0), 0)
   )
+
+  if (!config.touchMode) {
+    return mouseup$.pipe(
+      filter(e => !isInSaladictExternal(e.target)),
+      // if user click on a selected text,
+      // getSelection would reture the text before the highlight disappears
+      // delay to wait for selection get cleared
+      delay(10),
+      withLatestFrom(mousedown$, clickPeriodCount$),
+      map(([mouseup, mousedown, clickPeriodCount]) => {
+        const self = isInDictPanel(mousedown.target)
+
+        if (config.noTypeField && isTypeField(mouseup.target)) {
+          return self
+        }
+
+        const selection = window.getSelection()
+        const text = getTextFromSelection(selection)
+
+        if (!checkSupportedLangs(config.language, text)) {
+          return self
+        }
+
+        return {
+          word: newSelectionWord({
+            text,
+            context: getSentenceFromSelection(selection)
+          }),
+          self,
+          dbClick: clickPeriodCount >= 2,
+          mouseX: mouseup.clientX,
+          mouseY: mouseup.clientY,
+          shiftKey: !!mouseup['shiftKey'],
+          ctrlKey: !!mouseup['ctrlKey'],
+          metaKey: !!mouseup['metaKey']
+        }
+      }),
+      distinctUntilChanged((oldVal, newVal) => {
+        // (Ignore this rule if it is a double click.)
+        // Same selection. This could be caused by other widget on the page
+        // that uses preventDefault which stops selection being cleared when clicked.
+        // Ignore it so that the panel won't follow.
+        return (
+          typeof newVal !== 'boolean' &&
+          typeof oldVal !== 'boolean' &&
+          !newVal.dbClick &&
+          oldVal.word.text === newVal.word.text &&
+          oldVal.word.context === newVal.word.context
+        )
+      })
+    )
+  }
 
   const isMouseDown$ = merge(
     mapTo(true)(mousedown$),
