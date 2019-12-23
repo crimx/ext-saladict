c9b6a0d99ab6444176b1486de40688f246cdb083
crimx <straybugs@gmail.com>
2019-12-23 22:58:45 +0800

    refactor(wordeditor): abstract panel

---
+++ src/content/components/WordEditor/WordEditorPanel.stories.tsx
@@ -0,0 +1,78 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+import { jsxDecorator } from 'storybook-addon-jsx'
+import { withPropsTable } from 'storybook-addon-react-docgen'
+import { withKnobs, boolean, number, text } from '@storybook/addon-knobs'
+import { WordEditorPanel } from './WordEditorPanel'
+import {
+  withLocalStyle,
+  withSideEffect,
+  mockRuntimeMessage,
+  getThemeStyles
+} from '@/_helpers/storybook'
+import faker from 'faker'
+
+storiesOf('Content Scripts|WordEditor', module)
+  .addDecorator(withPropsTable)
+  .addDecorator(jsxDecorator)
+  .addDecorator(withKnobs)
+  .addDecorator(
+    withSideEffect(
+      mockRuntimeMessage(async message => {
+        action(message.type)(message['payload'])
+      })
+    )
+  )
+  .add(
+    'WordEditorPanel',
+    () => {
+      const darkMode = boolean('Dark Mode', false)
+      const colors = getThemeStyles(darkMode)
+
+      return (
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            padding: '20px 0'
+          }}
+        >
+          <WordEditorPanel
+            width={number('Panel Width', 450)}
+            colors={colors}
+            btns={
+              boolean('With Buttons', true)
+                ? [
+                    {
+                      type: 'normal',
+                      title: 'Normal Button',
+                      onClick: action('Normal button clicked')
+                    },
+                    {
+                      type: 'primary',
+                      title: 'Primary Button',
+                      onClick: action('Primary button clicked')
+                    }
+                  ]
+                : undefined
+            }
+            title={text('Title', faker.random.word())}
+            onClose={action('Close')}
+          >
+            <div style={{ padding: 10 }}>
+              {text('Content', faker.lorem.paragraphs())
+                .split('\n')
+                .map((paragraph, index) => (
+                  <p key={index}>{paragraph}</p>
+                ))}
+            </div>
+          </WordEditorPanel>
+        </div>
+      )
+    },
+    {
+      decorators: [withLocalStyle(require('./WordEditorPanel.scss'))],
+      jsx: { skip: 1 }
+    }
+  )
