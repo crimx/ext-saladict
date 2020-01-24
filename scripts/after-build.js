929b37240f2893245cf3e8f049825c622a710f7d
crimx <straybugs@gmail.com>
2020-01-24 10:37:39 +0800

    chore: update ff pack script

--- scripts/after-build.js
+++ scripts/after-build.js
@@ -1,5 +1,16 @@
 const fs = require('fs-extra')
 const path = require('path')
 
-// FF policy
-fs.remove(path.join(__dirname, '../build/firefox/assets/fanyi.youdao.2.0'))
+main()
+
+async function main() {
+  // FF policy
+  await fs.remove(
+    path.join(__dirname, '../build/firefox/assets/fanyi.youdao.2.0')
+  )
+  // Stop FF extension check errors
+  await fs.outputFile(
+    path.join(__dirname, '../build/firefox/assets/fanyi.youdao.2.0/main.js'),
+    ''
+  )
+}
