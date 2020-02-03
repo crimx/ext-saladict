f667a5cfdcab14a47a7df72dd6e05ac809475c11
crimx <straybugs@gmail.com>
2020-02-03 16:47:01 +0800

    refactor: add edge id

--- src/_helpers/integrity.ts
+++ src/_helpers/integrity.ts
@@ -1,3 +1,5 @@
 export const isExtTainted =
   browser.runtime.id !== atob('Y2Rvbm5tZmZrZGFvYWpma25vZWVlY21jaGlicG1rbWc=') &&
-  browser.runtime.id !== atob('c2FsYWRpY3RAY3JpbXguY29t')
+  browser.runtime.id !== atob('c2FsYWRpY3RAY3JpbXguY29t') &&
+  browser.runtime.id !==
+    atob('ZGEwMDlmN2EtOWY1Ni00YTRhLTkzOTAtMmVlYTAxNjZiM2U0')
