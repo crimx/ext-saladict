c2a9c764570d4d04b050c56ec76d316b4558e9ca
crimx <straybugs@gmail.com>
2020-03-20 21:41:58 +0800

    refactor: clean code

--- src/_helpers/wordoftheday.ts
+++ src/_helpers/wordoftheday.ts
@@ -2,25 +2,28 @@ import { fetchDirtyDOM } from '@/_helpers/fetch-dom'
 import { first } from '@/_helpers/promise-more'
 import { handleNoResult, getText } from '@/components/dictionaries/helpers'
 
-export function getWordOfTheDay (): Promise<string> {
-  return first([
-    getWebsterWordOfTheDay(),
-    getDictionaryWordOfTheDay(),
-  ]).catch(() => 'salad')
+export async function getWordOfTheDay(): Promise<string> {
+  try {
+    return await first([getWebsterWordOfTheDay(), getDictionaryWordOfTheDay()])
+  } catch (e) {
+    return 'salad'
+  }
 }
 
-export function getWebsterWordOfTheDay (): Promise<string> {
-  return fetchDirtyDOM('https://www.merriam-webster.com/word-of-the-day')
-    .then<string>(doc => {
-      const text = getText(doc, 'title')
-      return (text.match(/Word of the Day: (.+) \| Merriam-Webster/) || ['', ''])[1] || handleNoResult()
-    })
+export async function getWebsterWordOfTheDay(): Promise<string> {
+  const doc = await fetchDirtyDOM(
+    'https://www.merriam-webster.com/word-of-the-day'
+  )
+  const text = getText(doc, 'title')
+  const matchResult = text.match(/Word of the Day: (.+) \| Merriam-Webster/)
+  return (matchResult && matchResult[1]) || handleNoResult()
 }
 
-export function getDictionaryWordOfTheDay (): Promise<string> {
-  return fetchDirtyDOM('https://www.dictionary.com/wordoftheday/')
-    .then<string>(doc => {
-      const text = getText(doc, 'title')
-      return (text.match(/Get the Word of the Day - (.+) \| Dictionary\.com/) || ['', ''])[1] || handleNoResult()
-    })
+export async function getDictionaryWordOfTheDay(): Promise<string> {
+  const doc = await fetchDirtyDOM('https://www.dictionary.com/wordoftheday/')
+  const text = getText(doc, 'title')
+  const matchResult = text.match(
+    /Get the Word of the Day - (.+) \| Dictionary\.com/
+  )
+  return (matchResult && matchResult[1]) || handleNoResult()
 }
