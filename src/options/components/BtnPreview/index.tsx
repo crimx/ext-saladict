646c283862bbe43c0150410901e65be26a8cbbfa
crimx <straybugs@gmail.com>
2020-04-08 01:05:01 +0800

    refactor: add open panel action

--- src/options/components/BtnPreview/index.tsx
+++ src/options/components/BtnPreview/index.tsx
@@ -1,12 +1,12 @@
 import React, { FC } from 'react'
-import { useSelector } from 'react-redux'
+import { Dispatch } from 'redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { CSSTransition } from 'react-transition-group'
 import { Button } from 'antd'
 import { useTranslate } from '@/_helpers/i18n'
-import { message } from '@/_helpers/browser-api'
 import { newWord } from '@/_helpers/record-manager'
 import { getWordOfTheDay } from '@/_helpers/wordoftheday'
-import { StoreState } from '@/content/redux/modules'
+import { StoreState, StoreAction } from '@/content/redux/modules'
 import { PreviewIcon } from './PreviewIcon'
 
 import './_style.scss'
@@ -14,6 +14,7 @@ import './_style.scss'
 export const BtnPreview: FC = () => {
   const { t } = useTranslate('options')
   const show = !useSelector(pickIsShowDictPanel)
+  const dispatch = useDispatch<Dispatch<StoreAction>>()
 
   return (
     <CSSTransition
@@ -31,7 +32,18 @@ export const BtnPreview: FC = () => {
           shape="circle"
           size="large"
           icon={<PreviewIcon />}
-          onClick={openDictPanel}
+          onClick={async e => {
+            const { x, width } = e.currentTarget.getBoundingClientRect()
+            // panel will adjust the postion itself
+            dispatch({ type: 'OPEN_PANEL', payload: { x: x + width, y: 80 } })
+            dispatch({
+              type: 'SEARCH_START',
+              payload: {
+                word: newWord({ text: await getWordOfTheDay() }),
+                noHistory: true
+              }
+            })
+          }}
         />
       </div>
     </CSSTransition>
@@ -42,23 +54,4 @@ export const BtnPreviewMemo = React.memo(BtnPreview)
 
 function pickIsShowDictPanel(state: StoreState): boolean {
   return state.isShowDictPanel
-}
-
-async function openDictPanel(e: React.MouseEvent<HTMLButtonElement>) {
-  const { x, width } = e.currentTarget.getBoundingClientRect()
-  message.self.send({
-    type: 'SELECTION',
-    payload: {
-      word: newWord({ text: await getWordOfTheDay() }),
-      self: true, // selection inside dict panel is always avaliable
-      instant: true,
-      mouseX: x + width,
-      mouseY: 80,
-      dbClick: true,
-      shiftKey: true,
-      ctrlKey: true,
-      metaKey: true,
-      force: true
-    }
-  })
 }
