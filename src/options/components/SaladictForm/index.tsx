7277e801120d31ebfc90dfad92d63d71e315aba8
crimx <straybugs@gmail.com>
2020-04-13 01:28:25 +0800

    refactor(options): add recursive form items

--- src/options/components/SaladictForm/index.tsx
+++ src/options/components/SaladictForm/index.tsx
@@ -1,23 +1,43 @@
-import React, { FC, useContext } from 'react'
+import React, { FC, useContext, ReactNode, useMemo } from 'react'
 import { Form, Button, Modal } from 'antd'
 import { FormItemProps } from 'antd/lib/form'
 import { ExclamationCircleOutlined, BlockOutlined } from '@ant-design/icons'
 import get from 'lodash/get'
-import { map, withLatestFrom } from 'rxjs/operators'
-import { useObservableState, useObservable } from 'observable-hooks'
+import mapValues from 'lodash/mapValues'
+import { startWith, map, distinctUntilChanged } from 'rxjs/operators'
+import { useObservableState } from 'observable-hooks'
 import { resetConfig } from '@/_helpers/config-manager'
 import { resetAllProfiles } from '@/_helpers/profile-manager'
 import { useTranslate } from '@/_helpers/i18n'
 import { isFirefox } from '@/_helpers/saladict'
 import { openURL } from '@/_helpers/browser-api'
-import { config$$, profile$$, FormDirtyContext } from '@/options/data'
+import { GlobalsContext } from '@/options/data'
 import { formItemLayout, formItemHeadLayout } from '@/options/helpers/layout'
 import { uploadResult$$, upload } from '@/options/helpers/upload'
+import shallowEqual from 'shallowequal'
 
 import './_style.scss'
 
-export interface SaladictFormItem extends Omit<FormItemProps, 'name'> {
-  name: string
+interface FieldValues {
+  [name: string]: any
+}
+
+interface FieldShow {
+  [name: string]: boolean
+}
+
+export interface SaladictFormItem
+  extends Omit<FormItemProps, 'name' | 'children'> {
+  /** Must set name or key. Set name if the item has value. */
+  name?: string
+  /** Must set name or key. Set key if the item does not carry value. */
+  key?: string
+  /** Hide item based on other fields */
+  hide?: (values: FieldValues) => boolean
+  /** Nested items. Must set items or children. */
+  items?: SaladictFormItem[]
+  /** Must set items or children. */
+  children?: ReactNode
 }
 
 export interface SaladictFormProps {
@@ -26,34 +46,105 @@ export interface SaladictFormProps {
 
 export const SaladictForm: FC<SaladictFormProps> = props => {
   const { t, i18n, ready } = useTranslate(['options', 'common'])
-  const dirtyRef = useContext(FormDirtyContext)
+  const globals = useContext(GlobalsContext)
   const { loading: uploading } = useObservableState(uploadResult$$, {
     loading: false
   })
 
-  const initialValues = useObservableState(
-    useObservable(
-      inputs$ =>
-        inputs$.pipe(
-          withLatestFrom(config$$, profile$$),
-          map(([[items], config, profile]) => {
-            const data = { config, profile }
-            return items.reduce((o, item) => {
-              const value = get(data, item.name, data)
-              if (value !== data) {
-                o[item.name] = value
-              } else if (process.env.DEBUG) {
-                console.warn(
-                  new Error('Missing value for form key: ' + item.name)
-                )
-              }
-              return o
-            }, {} as { [index: string]: any })
-          })
-        ),
-      [props.items]
+  function extractInitial(
+    items: SaladictFormItem[],
+    result: {
+      initialValues: { [index: string]: any }
+      hideFieldFns: { [index: string]: (values: FieldValues) => boolean }
+    } = { initialValues: {}, hideFieldFns: {} }
+  ): { [index: string]: any } {
+    for (const item of items) {
+      if (item.items) {
+        extractInitial(item.items, result)
+      } else {
+        const name = (item.key || item.name)!
+
+        if (item.hide) {
+          result.hideFieldFns[name] = item.hide
+        }
+
+        const value = get(globals, name, globals)
+        if (value !== globals) {
+          result.initialValues[name] = value
+        } else if (process.env.DEBUG) {
+          console.warn(new Error('Missing value for form key: ' + item.name))
+        }
+      }
+    }
+    return result
+  }
+
+  const { initialValues, hideFieldFns } = useMemo(
+    () => extractInitial(props.items),
+    [props.items]
+  )
+
+  const [hideFields, setHideFields] = useObservableState<
+    FieldShow,
+    FieldValues,
+    true
+  >(input$ =>
+    input$.pipe(
+      map(values => mapValues(hideFieldFns, hide => hide(values))),
+      startWith(mapValues(hideFieldFns, hide => hide(initialValues))),
+      distinctUntilChanged(shallowEqual)
     )
   )
+
+  function genFormItems(items: SaladictFormItem[]) {
+    return items.map(item => {
+      const name = (item.key || item.name)!
+      const isProfile = name.startsWith('profile.')
+
+      if (ready && i18n.exists(`options:${name}`)) {
+        item.label = isProfile ? (
+          <>
+            <BlockOutlined style={{ color: '#f5222d', marginRight: '0.5em' }} />
+            {t(name)}
+          </>
+        ) : (
+          t(name)
+        )
+      }
+
+      const help = `options:${name}_help`
+      if (ready && i18n.exists(help)) {
+        item.help = t(help)
+      }
+
+      const extra = `options:${name}_extra`
+      if (ready) {
+        if (i18n.exists(extra)) {
+          item.extra = t(extra)
+        } else if (isProfile) {
+          item.extra = t('profile.opt.item_extra')
+        }
+      }
+
+      let { className, hide, children, items: subItems, ...itemProps } = item
+      if (hideFields[name]) {
+        className = className ? className + ' saladict-hide' : 'saladict-hide'
+      }
+
+      return (
+        <Form.Item key={name} {...itemProps} className={className}>
+          {subItems ? genFormItems(subItems) : children!}
+        </Form.Item>
+      )
+    })
+  }
+
+  const formItems = useMemo(() => genFormItems(props.items), [
+    ready,
+    i18n.language,
+    hideFields,
+    props.items
+  ])
 
   return (
     <Form
@@ -62,37 +153,10 @@ export const SaladictForm: FC<SaladictFormProps> = props => {
       initialValues={initialValues}
       onValuesChange={(_, values) => {
         ;(globals as GlobalsContext).dirty = true
-        setShowFields(genShowFields(values))
+        setHideFields(values)
       }}
     >
-      {props.items.map(item => {
-        const isProfile = item.name.startsWith('profile.')
-
-        item.label = isProfile ? (
-          <>
-            <BlockOutlined style={{ color: '#f5222d', marginRight: '0.5em' }} />
-            {t(item.name)}
-          </>
-        ) : (
-          t(item.name)
-        )
-
-        const help = `options:${item.name}_help`
-        if (ready && i18n.exists(help)) {
-          item.help = t(help)
-        }
-
-        const extra = `options:${item.name}_extra`
-        if (ready) {
-          if (i18n.exists(extra)) {
-            item.extra = t(extra)
-          } else if (isProfile) {
-            item.extra = t('profile.opt.item_extra')
-          }
-        }
-
-        return <Form.Item key={item.name} {...item} />
-      })}
+      {formItems}
       <Form.Item {...formItemHeadLayout} className="saladict-form-btns">
         <Button type="primary" htmlType="submit" disabled={uploading}>
           {t('common:save')}
