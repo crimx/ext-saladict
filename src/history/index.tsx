10b2c8014a3c2045e9f6bc58ff124d6eff8be2b5
crimx <straybugs@gmail.com>
2020-04-26 00:33:36 +0800

    refactor: add transition on antd loading
    Closes #753

--- src/history/index.tsx
+++ src/history/index.tsx
@@ -4,13 +4,22 @@ import '@/selection'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { WordPage } from '@/components/WordPage'
-import { AntdRoot } from '@/components/AntdRoot'
+import { AntdRoot, switchAntdTheme } from '@/components/AntdRoot'
+import { createConfigStream } from '@/_helpers/config-manager'
 
 document.title = 'Saladict History'
 
-ReactDOM.render(
-  <AntdRoot path="/wordpage/history">
-    <WordPage area="history" />
-  </AntdRoot>,
-  document.getElementById('root')
-)
+const rendered = false
+
+createConfigStream().subscribe(async config => {
+  await switchAntdTheme(config.darkMode)
+
+  if (!rendered) {
+    ReactDOM.render(
+      <AntdRoot path="/wordpage/history">
+        <WordPage area="history" />
+      </AntdRoot>,
+      document.getElementById('root')
+    )
+  }
+})
--- src/notebook/index.tsx
+++ src/notebook/index.tsx
@@ -4,13 +4,24 @@ import '@/selection'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { WordPage } from '@/components/WordPage'
-import { AntdRoot } from '@/components/AntdRoot'
+import { AntdRoot, switchAntdTheme } from '@/components/AntdRoot'
+import { createConfigStream } from '@/_helpers/config-manager'
 
 document.title = 'Saladict Notebook'
 
-ReactDOM.render(
-  <AntdRoot path="/wordpage/notebook">
-    <WordPage area="notebook" />
-  </AntdRoot>,
-  document.getElementById('root')
-)
+let rendered = false
+
+createConfigStream().subscribe(async config => {
+  await switchAntdTheme(config.darkMode)
+
+  if (!rendered) {
+    ReactDOM.render(
+      <AntdRoot path="/wordpage/notebook">
+        <WordPage area="notebook" />
+      </AntdRoot>,
+      document.getElementById('root')
+    )
+
+    rendered = true
+  }
+})
