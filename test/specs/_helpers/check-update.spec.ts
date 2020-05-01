6582938c814a92b5ab36a8ae20b4ebdf6a77cc98
crimx <straybugs@gmail.com>
2020-05-01 11:35:34 +0800

    test: update check-update

--- test/specs/_helpers/check-update.spec.ts
+++ test/specs/_helpers/check-update.spec.ts
@@ -1,97 +1,49 @@
-import checkUpdate from '@/_helpers/check-update'
+import { checkUpdate } from '@/_helpers/check-update'
 import _fetchMock, { FetchMock } from 'jest-fetch-mock'
-import { browser } from '../../helper'
+import getDefaultConfig from '@/app-config'
 
 const fetchMock = _fetchMock as FetchMock
 
 describe('Check Update', () => {
   beforeAll(() => {
     window.fetch = fetchMock
-    browser.runtime.getManifest.returns({
-      version: '1.1.1'
-    })
   })
+
   beforeEach(() => {
     fetchMock.resetMocks()
+    window.appConfig = getDefaultConfig()
   })
 
-  it('Server Got Same Version (Not Available)', () => {
-    const responseObj = { tag_name: 'v1.1.1' }
-    const resolveSpy = jest.fn()
-    const rejectSpy = jest.fn()
-    const catchSpy = jest.fn()
+  const tests = [
+    ['Same', 'v1.1.1', 'v1.1.1', 0],
+    ['Newer Patch', 'v1.1.2', 'v1.1.0', 1],
+    ['Newer Minor', 'v1.2.1', 'v1.0.1', 2],
+    ['Newer Major', 'v2.1.1', 'v0.1.1', 3],
+    ['Older Patch', 'v1.1.0', 'v1.1.2', -1],
+    ['Older Minor', 'v1.0.1', 'v1.2.1', -2],
+    ['Older Major', 'v0.1.1', 'v2.1.1', -3]
+  ] as const
 
-    fetchMock.mockResponseOnce(JSON.stringify(responseObj))
+  tests.forEach(([title, newerVersion, olderVersion, diff]) => {
+    it(title, async () => {
+      const responseObj = { version: newerVersion }
+      const resolveSpy = jest.fn()
+      const rejectSpy = jest.fn()
+      const catchSpy = jest.fn()
 
-    const p = checkUpdate()
-      .then(resolveSpy, rejectSpy)
-      .catch(catchSpy)
-      .then(() => {
-        expect(resolveSpy).toHaveBeenCalledTimes(1)
-        expect(rejectSpy).toHaveBeenCalledTimes(0)
-        expect(catchSpy).toHaveBeenCalledTimes(0)
-        expect(resolveSpy).toBeCalledWith(
-          expect.objectContaining({
-            isAvailable: false
-          })
-        )
-      })
-    expect(fetchMock).toHaveBeenCalledTimes(1)
-    return p
-  })
-  ;[
-    ['Patch', 'v1.1.2', 'v1.1.0'],
-    ['Minor', 'v1.2.1', 'v1.0.1'],
-    ['Major', 'v2.1.1', 'v0.1.1']
-  ].forEach(([title, newerVersion, olderVersion]) => {
-    describe(title, () => {
-      it('New Version Available', () => {
-        const responseObj = { tag_name: newerVersion }
-        const resolveSpy = jest.fn()
-        const rejectSpy = jest.fn()
-        const catchSpy = jest.fn()
+      fetchMock.mockResponseOnce(JSON.stringify(responseObj))
 
-        fetchMock.mockResponseOnce(JSON.stringify(responseObj))
+      await checkUpdate(olderVersion.slice(1))
+        .then(resolveSpy, rejectSpy)
+        .catch(catchSpy)
 
-        const p = checkUpdate()
-          .then(resolveSpy, rejectSpy)
-          .catch(catchSpy)
-          .then(() => {
-            expect(resolveSpy).toHaveBeenCalledTimes(1)
-            expect(rejectSpy).toHaveBeenCalledTimes(0)
-            expect(catchSpy).toHaveBeenCalledTimes(0)
-            expect(resolveSpy).toBeCalledWith({
-              isAvailable: true,
-              info: responseObj
-            })
-          })
-        expect(fetchMock).toHaveBeenCalledTimes(1)
-        return p
-      })
-
-      it('Server Got Older Version (Not Available)', () => {
-        const responseObj = { tag_name: olderVersion }
-        const resolveSpy = jest.fn()
-        const rejectSpy = jest.fn()
-        const catchSpy = jest.fn()
-
-        fetchMock.mockResponseOnce(JSON.stringify(responseObj))
-
-        const p = checkUpdate()
-          .then(resolveSpy, rejectSpy)
-          .catch(catchSpy)
-          .then(() => {
-            expect(resolveSpy).toHaveBeenCalledTimes(1)
-            expect(rejectSpy).toHaveBeenCalledTimes(0)
-            expect(catchSpy).toHaveBeenCalledTimes(0)
-            expect(resolveSpy).toBeCalledWith(
-              expect.objectContaining({
-                isAvailable: false
-              })
-            )
-          })
-        expect(fetchMock).toHaveBeenCalledTimes(1)
-        return p
+      expect(fetchMock).toHaveBeenCalledTimes(1)
+      expect(resolveSpy).toHaveBeenCalledTimes(1)
+      expect(rejectSpy).toHaveBeenCalledTimes(0)
+      expect(catchSpy).toHaveBeenCalledTimes(0)
+      expect(resolveSpy).toBeCalledWith({
+        diff,
+        data: responseObj
       })
     })
   })
