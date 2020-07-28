ff92a03b7736467f1000315e5d40382a0e206628
crimx <straybugs@gmail.com>
2020-07-28 13:27:05 +0800

    refactor(sync-services): add webdav proxy warning
    #919

--- src/background/sync-manager/services/webdav/_locales/zh-TW.ts
+++ src/background/sync-manager/services/webdav/_locales/zh-TW.ts
@@ -4,7 +4,8 @@ export const locale: typeof _locale = {
   title: 'WebDAV 單字同步',
   error: {
     dir: '伺服器上“Saladict”目錄格式不正確，請檢查。',
-    download: '下載失敗。無法訪問 WebDAV 伺服器。',
+    download:
+      '下載失敗。無法訪問 WebDAV 伺服器。如啟用了瀏覽器代理請調整規則，不要代理 WebDAV 伺服器。',
     internal: '無法儲存。',
     missing: '伺服器上缺少“Saladict”目錄。',
     mkcol: '無法在伺服器建立“Saladict”目錄。請手動在伺服器上建立。',
