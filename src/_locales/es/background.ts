3a8661dc601c5e695e9ca3771328f401a0e3679b
daniel r <daniel@informaticaremota.es>
2023-04-14 19:53:31 +0000

    background.ts translation to spanish

---
+++ src/_locales/es/background.ts
@@ -0,0 +1,11 @@
+import { locale as _locale } from '../zh-CN/background'
+
+export const locale: typeof _locale = {
+  app: {
+    off: 'Saladict desactivado. (El panel de búsqueda rápida sigue disponible)',
+    tempOff:
+      'Saladict desactivado en la pestaña actual. (El panel de búsqueda rápida sigue disponible)',
+    unsupported:
+      'El panel Saladict incrustado no es compatible con la pestaña actual. Utilice el panel Saladict independiente en su lugar.'
+  }
+}
