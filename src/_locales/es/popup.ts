8b190f56a09ee085cb04d303f2d8dde5c2b7ea40
daniel r <daniel@informaticaremota.es>
2023-04-15 11:33:22 +0000

    popup.ts file translated into Spanish

---
+++ src/_locales/es/popup.ts
@@ -0,0 +1,14 @@
+import { locale as _locale } from '../zh-CN/popup'
+
+export const locale: typeof _locale = {
+  title: 'Panel de acciones del navegador de Saladict',
+  app_active_title: 'Activar el traductor en línea',
+  app_temp_active_title: 'Desactivación temporal de la página',
+  instant_capture_pinned: ' (fijado) ',
+  instant_capture_title: 'Activar captura instantánea',
+  notebook_added: 'Añadido',
+  notebook_empty: 'No se ha encontrado ninguna selección en la página actual',
+  notebook_error: 'No se puede añadir el texto seleccionado al bloc de notas',
+  page_no_response: 'La pagina no responde',
+  qrcode_title: 'Qrcode de la página'
+}
