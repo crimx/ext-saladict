ca3f51823d15415190e18983fe7ffe88e01ba52b
danielrtic <130758768+danielrtic@users.noreply.github.com>
2023-04-15 14:41:29 +0200

    refactor: add translated Spanish language (#2017)
    * background.ts translation to spanish
    
    * common.ts translation to Spanish
    
    * content.ts translation to spanish
    
    * langcode.ts translation to Spanish
    
    * translation menus.ts to spanish
    
    * options.ts file translated into English
    
    * popup.ts file translated into Spanish
    
    * wordpage.ts file translated into Spanish

---
+++ src/_locales/es/common.ts
@@ -0,0 +1,75 @@
+import { locale as _locale } from '../zh-CN/common'
+
+export const locale: typeof _locale = {
+  add: 'Añadir',
+  delete: 'Eliminar',
+  save: 'Guardar',
+  cancel: 'CAncelar',
+
+  edit: 'Editar',
+  sort: 'Ordenar',
+  rename: 'Renombrar',
+
+  confirm: 'Confirmar',
+  changes_confirm: 'Cambios no guardados. ¿Cerrar de todas formas?',
+  delete_confirm: '¿Eliminar completamente el elemento?',
+
+  max: 'Max',
+  min: 'Min',
+
+  name: 'Nombre',
+  none: 'Ninguno',
+
+  enable: 'Activar',
+  enabled: 'Activado',
+  disabled: 'Desactivado',
+
+  blacklist: 'Lista negra',
+  whitelist: 'Lista blanca',
+
+  import: 'Importar',
+  export: 'Exportar',
+
+  lang: {
+    chinese: 'Chino',
+    chs: 'Chino',
+    deutsch: 'Alemán',
+    eng: 'Inglés',
+    english: 'Inglés',
+    french: 'Francés',
+    japanese: 'Japonés',
+    korean: 'Coreano',
+    matchAll: 'Coincidir con todos los caracteres',
+    minor: 'Menor',
+    others: 'Otros',
+    spanish: 'Español'
+  },
+
+  unit: {
+    mins: 'minutes',
+    ms: 'ms',
+    s: 's',
+    word: 'words'
+  },
+
+  note: {
+    word: 'Word',
+    trans: 'Translation',
+    note: 'Note',
+    context: 'Context',
+    contextCloze: 'Context Cloze',
+    date: 'Date',
+    srcTitle: 'Source Title',
+    srcLink: 'Source Link',
+    srcFavicon: 'Source Favicon'
+  },
+
+  profile: {
+    daily: 'Daily Mode',
+    sentence: 'Sentence Mode',
+    default: 'Default Mode',
+    scholar: 'Scholar Mode',
+    translation: 'Translation Mode',
+    nihongo: 'Japanese Mode'
+  }
+}
