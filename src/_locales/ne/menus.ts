3374ec3c1978abb6d0f783566943f7dc0b977a8a
sundeepdawadi <65609144+sundeepdwd@users.noreply.github.com>
2024-01-17 13:17:28 +0545

    feat(locales): added nepali translation (#2153)

---
+++ src/_locales/ne/menus.ts
@@ -0,0 +1,42 @@
+import { locale as _locale } from '../zh-CN/menus'
+
+export const locale: typeof _locale = {
+  baidu_page_translate: 'बाइडु पृष्ठ अनुवाद',
+  baidu_search: 'बाइडु खोजी',
+  bing_dict: 'बिङ शब्दकोश',
+  bing_search: 'बिङ खोजी',
+  caiyuntrs: 'Lingocloud पृष्ठ अनुवाद',
+  cambridge: 'क्याम्ब्रिज',
+  copy_pdf_url: 'पीडीएफ यूआरएल क्लिपबोर्डमा प्रतिलिपि गर्नुहोस्',
+  dictcn: 'Dictcn',
+  etymonline: 'Etymonline',
+  google_cn_page_translate: 'Google cn पृष्ठ अनुवाद',
+  google_page_translate: 'Google पृष्ठ अनुवाद',
+  google_search: 'Google खोजी',
+  google_translate: 'Google अनुवाद',
+  google_cn_translate: 'Google.cn अनुवाद',
+  guoyu: '國語辭典',
+  history_title: 'खोज इतिहास',
+  iciba: 'iciba',
+  liangan: '兩岸詞典',
+  longman_business: 'Longman Business',
+  manual_title: 'मैनुअल',
+  merriam_webster: 'Merriam Webster',
+  microsoft_page_translate: 'Microsoft पृष्ठ अनुवाद',
+  notebook_title: 'नयाँ शब्द सूची',
+  notification_youdao_err:
+    'यूडाओ पृष्ठ अनुवाद 2.0ले प्रतिक्रिया दिएन ।\nसलाडिक्ट यस पृष्ठमा पहुँच प्राप्त गर्न सक्दैन।\nयदि यूडाओ प्यानल देखाइएको छ भने यो सन्देश अवहेलना गर्नुहोस्।',
+  oxford: 'अक्सफोर्ड',
+  page_permission_err:
+    'सलाडिक्ट "{{name}}" यस पृष्ठमा पहुँच प्राप्त गर्न अनुमति छैन।',
+  page_translations: 'पृष्ठ अनुवाद',
+  saladict: 'सलाडिक्ट',
+  saladict_standalone: 'सलाडिक्ट स्ट्यान्डअलोन प्यानल',
+  sogou: 'सोगो अनुवाद',
+  sogou_page_translate: 'सोगो पृष्ठ अनुवाद',
+  termonline: 'Termonline',
+  view_as_pdf: 'पीडीएफ भिउमा खोल्नुहोस्',
+  youdao: 'यौडाओ',
+  youdao_page_translate: 'यौडाओ पृष्ठ अनुवाद',
+  youglish: 'यौग्लिश'
+}
